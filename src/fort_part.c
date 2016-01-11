a491d8685e59618c992b739e7841a372b43c0e65
Natalja Rakowsky <natalja.rakowsky@awi.de>
2016-01-11 13:24:02 +0000

    Little less weight on 3D-nodes

--- src/fort_part.c
+++ src/fort_part.c
@@ -52,6 +52,7 @@ void partit(idx_t *n, idx_t *ptr, idx_t *adj, idx_t *wgt, idx_t *np, idx_t *part
 
   opt[METIS_OPTION_CONTIG]    = 0;  /* 1: contiguous partitions, please */
                                    /* With more weights, this makes the partitions uglier... */
+                                   /* Ignored by METIS_PartGraphRecursive */
 
   opt[METIS_OPTION_OBJTYPE]   = METIS_OBJTYPE_CUT; /* Minimize edge cut. */
 
@@ -71,7 +72,27 @@ void partit(idx_t *n, idx_t *ptr, idx_t *adj, idx_t *wgt, idx_t *np, idx_t *part
   /* max(#3D-nodes)/min(#3D-nodes), with METIS_PartGraphKway. */
   /* But: lower value -> lower maximum number of nodes in all partitions. This it what counts in the end! */
   /* Far better load balancing with METIS_PartGraphRecursive. */
- 
+
+/*   opt[METIS_OPTION_CTYPE] = METIS_CTYPE_SHEM; */
+/*   opt[METIS_OPTION_IPTYPE] = METIS_IPTYPE_RANDOM; */
+/*   opt[METIS_OPTION_RTYPE] = METIS_RTYPE_SEP2SIDED; */
+  
+  /* Available Options: */
+  /* METIS PartGraphRecursive   METIS PartGraphKway */
+  /* METIS_OPTION_OBJTYPE,  -           x */
+  /* METIS_OPTION_CTYPE,    x           x */
+  /* METIS_OPTION_IPTYPE,   x           x */
+  /* METIS_OPTION_RTYPE,    x           x */
+  /* METIS_OPTION_NO2HOP,   x           x */
+  /* METIS_OPTION_NCUTS,    x           x */
+  /* METIS_OPTION_NITER,    x           x */
+  /* METIS_OPTION_SEED,     x           x */
+  /* METIS_OPTION_UFACTOR,  x           x */
+  /* METIS_OPTION_MINCONN,  -           x */
+  /* METIS_OPTION_CONTIG,   -           x */
+  /* METIS_OPTION_NUMBERING x           x */
+  /* METIS_OPTION_DBGLVL    x           x */
+
   if (wgt_type == 0) {
     ncon = 1;   
     printf("Distribution weight: 2D nodes\n"); 
@@ -85,12 +106,12 @@ void partit(idx_t *n, idx_t *ptr, idx_t *adj, idx_t *wgt, idx_t *np, idx_t *part
     ierr = METIS_PartGraphRecursive(n,&ncon,ptr,adj,wgt, NULL,NULL,np,NULL,NULL,opt,&ec,part);
     
   } else  {
-    /*  quasi Default */
+    /*    quasi Default */
     ncon=2;
     wgt_2d3d = (idx_t*) malloc(2* *n *sizeof(idx_t));
     for (i=0; i<*n; i++){
       wgt_2d3d[2*i]   = 1;
-      wgt_2d3d[2*i+1] = wgt[i]; 
+      wgt_2d3d[2*i+1] = wgt[i]+100; /* soften the 3D-criteria to allow for better general quality */
     }
     
     printf("Distribution weight: 2D and 3D nodes\n");
