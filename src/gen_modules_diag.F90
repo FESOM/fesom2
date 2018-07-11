c5eb74dde8d2d1020102cb8e2ae644afb3c50b08
Dmitry Sidorenko <Dmitry.Sidorenko@awi.de>
2018-07-11 18:02:40 +0200

    EKE budget analysis fixed and tested and surprisingly it works :)

--- src/gen_modules_diag.F90
+++ src/gen_modules_diag.F90
@@ -14,7 +14,8 @@ module diagnostics
 
   private
   public :: compute_diagnostics, ldiag_solver, rhs_diag, lcurt_stress_surf, curl_stress_surf, ldiag_curl_vel3, curl_vel3, ldiag_energy, wzmid, wzmidrho, rho, &
-            u_x_u, u_x_v, v_x_v, v_x_w, u_x_w, dudx, dudy, dvdx, dvdy, dudz, dvdz, utau_surf, utau_bott, av_dudz_sq
+            u_x_u, u_x_v, v_x_v, v_x_w, u_x_w, dudx, dudy, dvdx, dvdy, dudz, dvdz, utau_surf, utau_bott, av_dudz_sq, &
+            taux_nod, tauy_nod, tbotx_nod, tboty_nod, av_nod, u_bott, v_bott
 
   ! Arrays used for diagnostics, some shall be accessible to the I/O
   ! 1. solver diagnostics: A*x=rhs? 
@@ -26,12 +27,12 @@ module diagnostics
   real(kind=8),  save, allocatable, target      :: u_x_u(:,:), u_x_v(:,:), v_x_v(:,:), v_x_w(:,:), u_x_w(:,:)
   real(kind=8),  save, allocatable, target      :: dudx(:,:), dudy(:,:), dvdx(:,:), dvdy(:,:), dudz(:,:), dvdz(:,:)
   real(kind=8),  save, allocatable, target      :: utau_surf(:), utau_bott(:), av_dudz_sq(:)
-
+  real(kind=8),  save, allocatable, target      :: taux_nod(:), tauy_nod(:), tbotx_nod(:), tboty_nod(:), av_nod(:,:), u_bott(:), v_bott(:)
 
   logical                                       :: ldiag_solver     =.false.
   logical                                       :: lcurt_stress_surf=.false.
   logical                                       :: ldiag_curl_vel3  =.false.
-  logical                                       :: ldiag_energy     =.false.
+  logical                                       :: ldiag_energy     =.true.
   contains
 
 ! ==============================================================
@@ -158,7 +159,7 @@ subroutine diag_energy(mode)
   integer, intent(in)           :: mode
   logical, save                 :: firstcall=.true.
   integer            :: n, nz, k, i, elem, nzmax, elnodes(3)
-  real(kind=WP)      :: ux, vx, uy, vy, tvol, rval(2), Av_nod(nl-1)
+  real(kind=WP)      :: ux, vx, uy, vy, tvol, rval(2)
   real(kind=WP)      :: stress_surf_n(2), stress_bott_n(2)
   real(kind=WP)      :: geo_grad_x(3), geo_grad_y(3), geo_u(3), geo_v(3)
 
@@ -168,6 +169,8 @@ subroutine diag_energy(mode)
      allocate(u_x_u(nl-1, myDim_nod2D), u_x_v(nl-1, myDim_nod2D), v_x_v(nl-1, myDim_nod2D), v_x_w(nl-1, myDim_nod2D), u_x_w(nl-1, myDim_nod2D))
      allocate(dudx(nl-1, myDim_nod2D), dudy(nl-1, myDim_nod2D), dvdx(nl-1, myDim_nod2D), dvdy(nl-1, myDim_nod2D), dudz(nl-1, myDim_nod2D), dvdz(nl-1, myDim_nod2D))
      allocate(utau_surf(myDim_nod2D), utau_bott(myDim_nod2D), av_dudz_sq(myDim_nod2D))
+     allocate(Av_nod(nl-1, myDim_nod2D), u_bott(myDim_nod2D), v_bott(myDim_nod2D))
+     allocate(taux_nod(myDim_nod2D), tauy_nod(myDim_nod2D), tbotx_nod(myDim_nod2D), tboty_nod(myDim_nod2D))
      wzmid=0.
      rho  =0.
      wzmidrho=0.
@@ -183,6 +186,17 @@ subroutine diag_energy(mode)
      dudz=0.
      dvdz=0.
      av_dudz_sq=0.
+
+     taux_nod=0.
+     tauy_nod=0.
+     tbotx_nod=0.
+     tboty_nod=0.
+
+     av_nod=0.
+     u_bott=0.
+     v_bott=0.
+
+
      firstcall=.false.
      if (mode==0) return
   end if
@@ -206,12 +220,18 @@ subroutine diag_energy(mode)
 
   utau_surf=0.
   utau_bott=0.
+  taux_nod =0.
+  tauy_nod =0.
+  tbotx_nod =0.
+  tboty_nod =0.
+  u_bott   =0.
+  v_bott   =0.
+  Av_nod   =0.
   ! this loop might be very expensive
   DO n=1, myDim_nod2D
      nzmax=nlevels_nod2D(n)
      stress_surf_n=0.
      stress_bott_n=0.
-     Av_nod=0.0_WP
      DO nz=1, nzmax-1
         tvol=0.0_WP
         ux  =0.0_WP
@@ -223,19 +243,26 @@ subroutine diag_energy(mode)
 	   elnodes=elem2D_nodes(:, elem)
            if (nz==1) then
               rval=stress_surf(:,elem)/density_0*elem_area(elem)
-              stress_surf_n = stress_surf_n+rval/Av(2, elem)   !accumulation of: du/dz at the surface (du/dz=tau/Av)
-              utau_surf(n)=utau_surf(n)+sum(rval*UV(:,1,elem)) !a scalar product tau_surface times u 
-                                                               !shall rotation be considered? (scalar product)
+              stress_surf_n = stress_surf_n+rval/Av(2, elem)      !accumulation of: du/dz at the surface (du/dz=tau/Av)
+              utau_surf(n)=utau_surf(n)+sum(rval(:)*UV(:,1,elem)) !a scalar product tau_surface times u 
+                                                                  !shall rotation be considered? (scalar product)
+              taux_nod(n)=taux_nod(n)+rval(1)
+              tauy_nod(n)=tauy_nod(n)+rval(2)
            end if
            if (nlevels(elem)-1 == nz) then
               rval=C_d*sqrt(UV(1, nz,elem)**2+ UV(2, nz, elem)**2)*UV(:,nz,elem)*elem_area(elem)
               stress_bott_n = stress_bott_n-rval/Av(nz, elem)  !accumulation of: du/dz at the bottom (du/dz=tau/Av)
               utau_bott(n)=utau_bott(n)-sum(rval*UV(:,nz,elem))!a scalar product tau_bottom times u 
                                                                !shall rotation be considered? (scalar product)
+              u_bott(n)=u_bott(n)+UV(1,nz,elem)*elem_area(elem)
+              v_bott(n)=v_bott(n)+UV(2,nz,elem)*elem_area(elem)
+               
+              tbotx_nod(n)=tbotx_nod(n)-rval(1)
+              tboty_nod(n)=tboty_nod(n)-rval(2)
            end if
            if (nlevels(elem)-1 < nz) cycle
-           Av_nod(nz)=Av_nod(nz)+Av(nz, elem)*elem_area(elem)  !we will need Av at nodes later
-           geo_grad_x=gradient_sca(1:3,elem)                   !reassign vector arrays to make the rotation possible (if required)
+           Av_nod(nz, n)=Av_nod(nz, n)+Av(nz+1, elem)*elem_area(elem)!we will need Av at nodes later
+           geo_grad_x=gradient_sca(1:3,elem)                       !reassign vector arrays to make the rotation possible (if required)
            geo_grad_y=gradient_sca(4:6,elem)
            geo_u=Unode(1,nz,elnodes)
            geo_v=Unode(2,nz,elnodes)
@@ -251,24 +278,36 @@ subroutine diag_energy(mode)
            uy=uy+sum(geo_grad_y*geo_u)*elem_area(elem)
            vy=vy+sum(geo_grad_y*geo_v)*elem_area(elem)
         END DO
-        Av_nod(nz)=Av_nod(nz)/tvol !convert the accumulation over layers to actual nodal values
-        dudx(nz,n)=ux/tvol
+        Av_nod(nz, n)=Av_nod(nz, n)/tvol !convert the accumulation over layers to actual nodal values
+        dudx(nz,n)=ux/tvol!/area(nz, n)/3.
         dvdx(nz,n)=vx/tvol
         dudy(nz,n)=uy/tvol
         dvdy(nz,n)=vy/tvol
      END DO
 
-     Av_nod(1)   =Av_nod(2)        !convert the accumulation over surface to actual surface values at nodes
-     stress_surf_n=stress_surf_n/area(1, n)*3. !*3. because of full areas were used in the loop
-     stress_bott_n=stress_bott_n/area(1, n)*3.
-     utau_surf(n)=utau_surf(n)/area(1, n)*3.
-     utau_bott(n)=utau_bott(n)/area(1, n)*3.
+!    Av_nod(1, n)   =Av_nod(2, n)              !convert the accumulation over surface to actual surface values at nodes
+     stress_surf_n=stress_surf_n/area(1, n)/3. !*3. because of full areas were used in the loop
+     stress_bott_n=stress_bott_n/area(1, n)/3.
+     utau_surf(n)=utau_surf(n)/area(1, n)/3.
+     utau_bott(n)=utau_bott(n)/area(1, n)/3.
+     
+     taux_nod(n)=taux_nod(n)/area(1, n)/3.
+     tauy_nod(n)=tauy_nod(n)/area(1, n)/3.
+
+     tbotx_nod(n)=tbotx_nod(n)/area(1, n)/3.
+     tboty_nod(n)=tboty_nod(n)/area(1, n)/3.
+
+     u_bott(n)=u_bott(n)/area(1, n)/3.
+     v_bott(n)=v_bott(n)/area(1, n)/3.
 
      if (rotated_grid) then
         call vector_r2g(stress_surf_n(1), stress_surf_n(2), coord_nod2D(1, n), coord_nod2D(2, n), 0)
         call vector_r2g(stress_bott_n(1), stress_bott_n(2), coord_nod2D(1, n), coord_nod2D(2, n), 0)
+        call vector_r2g(taux_nod(n),  tauy_nod(n),  coord_nod2D(1, n), coord_nod2D(2, n), 0)
+        call vector_r2g(tbotx_nod(n), tboty_nod(n), coord_nod2D(1, n), coord_nod2D(2, n), 0)
+        call vector_r2g(u_bott(n), v_bott(n), coord_nod2D(1, n), coord_nod2D(2, n), 0)
      end if
-
+         
      ! compute Z_n which is the mid depth of prisms (ALE supports changing layer thicknesses)
      zbar_n=0.0_WP
      Z_n=0.0_WP
@@ -286,11 +325,11 @@ subroutine diag_energy(mode)
 
      dudz(1, n)=stress_surf_n(1)
      dvdz(1, n)=stress_surf_n(2)
-     dudz(nl-1, n)=stress_bott_n(1)
-     dvdz(nl-1, n)=stress_bott_n(2)
+     dudz(nl-1, n)    =stress_bott_n(1)
+     dvdz(nl-1, n)    =stress_bott_n(2)
 
      !compute int(Av * (du/dz)^2)
-     av_dudz_sq(n)=sum((dudz(1:nzmax-1, n)**2+dvdz(1:nzmax-1, n)**2)*Av_nod(1:nzmax-1)* hnode_new(1:nzmax-1,n))
+     av_dudz_sq(n)=sum((dudz(1:nzmax-1, n)**2+dvdz(1:nzmax-1, n)**2)*Av_nod(1:nzmax-1, n)* hnode_new(1:nzmax-1,n))
   END DO  
 end subroutine diag_energy
 ! ==============================================================
