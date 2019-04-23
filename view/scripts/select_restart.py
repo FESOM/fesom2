8abfe693e2bc2e9ec80edb4c9b56c8d5872b9784
Nikolay Koldunov <koldunovn@gmail.com>
2019-04-23 09:58:28 +0200

    optionf for select_restart script

--- view/scripts/select_restart.py
+++ view/scripts/select_restart.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# This script will select one time step from the restart file 
+# This script will select one time step from the restart file
 # and put it in to a new restart file with only one time step.
 # This is needed to save space when copy very large restarts.
 # In this example the time step 6 (in python counting starts with zero)
-# will be selected. 
+# will be selected.
 # Note that there might be incompatability between different versions
-# of netCDF libraries, so in case of problems make sure you compile 
+# of netCDF libraries, so in case of problems make sure you compile
 # FESOM2 and xarray with the same version of netCDF
- 
+# for mistral create conda environment that have:
+# netcdf4==1.2.9
+# libnetcdf==4.4.1.1
+
+
 import xarray as xr
 
 # input files
-ocean = '/work/ollie/nkolduno/output_CORE_test1/fesom.1948.oce.restart.nc'
-ice   = '/work/ollie/nkolduno/output_CORE_test1/fesom.1948.ice.restart.nc'
+ocean = '/scratch/a/a270088/A8/fesom.2000.oce.restart.nc'
+ice   = '/scratch/a/a270088/A8/fesom.2000.ice.restart.nc'
 
 # output files
-ocean2 = '/work/ollie/nkolduno/output_CORE_test2/fesom.1948.oce.restart.nc'
-ice2   = '/work/ollie/nkolduno/output_CORE_test2/fesom.1948.ice.restart.nc'
+ocean2 = '/mnt/lustre01/work/ab0995/a270088/A8/restarts/fesom.2000.oce.restart.nc'
+ice2   = '/mnt/lustre01/work/ab0995/a270088/A8/restarts/fesom.2000.ice.restart.nc'
 
 fo = xr.open_dataset(ocean)
 fi = xr.open_dataset(ice)
