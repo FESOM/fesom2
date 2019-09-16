b589bd4de5af524923fca2e85c0513a5cc2758d6
Patrick Scholz <Patrick.Scholz@awi.de>
2019-09-16 13:49:46 +0200

    correct the Bug in the limitation of the GM diffusivity and improved the tunning of the vertical ferreira scaling for better climatology

--- src/oce_modules.F90
+++ src/oce_modules.F90
@@ -44,12 +44,15 @@ real(kind=WP)                 :: clim_decay, clim_growth
 logical                       :: ref_sss_local=.false.
 real(kind=WP)                 :: ref_sss=34.7
 logical                       :: Fer_GM =.false.  !flag for Ferrari et al. (2010) GM scheme
-real(kind=WP)                 :: K_GM=1000.
-logical			      :: scaling_Ferreira   =.true.
-logical			      :: scaling_Rossby     =.false. 
-logical			      :: scaling_resolution =.true.
-logical			      :: scaling_FESOM14    =.false.
-logical			      :: Redi               =.false.  !flag for Redi scheme
+real(kind=WP)                 :: K_GM_max = 3000.
+real(kind=WP)                 :: K_GM_min = 2.0
+integer                       :: K_GM_bvref = 2 ! 0...surface, 1...bottom mixlay, 2...mean over mixlay
+real(kind=WP)                 :: K_GM_resscalorder = 2.0 
+logical                       :: scaling_Ferreira   =.true.
+logical                       :: scaling_Rossby     =.false. 
+logical                       :: scaling_resolution =.true.
+logical                       :: scaling_FESOM14    =.false.
+logical                       :: Redi               =.false.  !flag for Redi scheme
 
 real(kind=WP)                 :: visc_sh_limit=5.0e-3      !for KPP, max visc due to shear instability
 real(kind=WP)                 :: diff_sh_limit=5.0e-3      !for KPP, max diff due to shear instability
@@ -128,7 +131,7 @@ character(20)                  :: which_pgf='shchepetkin'
 
  NAMELIST /oce_dyn/ C_d, A_ver, laplacian, A_hor, A_hor_max, Leith_c, tau_c, Div_c, Smag_c, &
                     biharmonic, Abh0, scale_area, mom_adv, free_slip, i_vert_visc, w_split, w_exp_max, SPP,&
-                    Fer_GM, K_GM, scaling_Ferreira, scaling_Rossby, scaling_resolution, scaling_FESOM14, Redi, visc_sh_limit, mix_scheme, Ricr, concv, which_pgf
+                    Fer_GM, K_GM_max,K_GM_min,K_GM_bvref,K_GM_resscalorder, scaling_Ferreira, scaling_Rossby, scaling_resolution, scaling_FESOM14, Redi, visc_sh_limit, mix_scheme, Ricr, concv, which_pgf
  NAMELIST /oce_tra/ diff_sh_limit, Kv0_const, double_diffusion, K_ver, K_hor, surf_relax_T, surf_relax_S, balance_salt_water, clim_relax, &
 		    ref_sss_local, ref_sss, i_vert_diff, tracer_adv, num_tracers, tracer_ID
 END MODULE o_PARAM  
@@ -357,7 +360,7 @@ real(kind=WP),allocatable :: mo(:,:),mixlength(:)
 real(kind=WP),allocatable :: bvfreq(:,:),mixlay_dep(:),bv_ref(:)
 
 real(kind=WP),         allocatable    :: fer_UV(:,:,:), fer_wvel(:,:)
-real(kind=WP), target, allocatable    :: fer_c(:), fer_K(:,:), fer_gamma(:,:,:)
+real(kind=WP), target, allocatable    :: fer_c(:), fer_scal(:), fer_K(:,:), fer_gamma(:,:,:)
 
 real(kind=WP),         allocatable    :: ice_rejected_salt(:)
 END MODULE o_ARRAYS
--- src/oce_setup_step.F90
+++ src/oce_setup_step.F90
@@ -279,13 +279,14 @@ sw_beta=0.0_WP
 sw_alpha=0.0_WP
 
 if (Fer_GM) then
-   allocate(fer_c(node_size), fer_gamma(2, nl, node_size), fer_K(nl, node_size))
+   allocate(fer_c(node_size),fer_scal(node_size), fer_gamma(2, nl, node_size), fer_K(nl, node_size))
    allocate(fer_wvel(nl, node_size), fer_UV(2, nl-1, elem_size))
    fer_gamma=0.0_WP
    fer_uv=0.0_WP
    fer_wvel=0.0_WP
    fer_K=500.
    fer_c=1.
+   fer_scal = 0.0
 end if
 
 if (SPP) then
