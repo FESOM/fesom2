f28d26f415bf56318d931e667e33948b431fe9e6
JanStreffing <Jan.Streffing@awi.de>
2020-05-08 09:07:56 +0200

    wip

--- src/ice_thermo_cpl.F90
+++ src/ice_thermo_cpl.F90
@@ -130,6 +130,7 @@ subroutine thermodynamics(mesh)
      call ice_growth
 #if defined (__oifs)
      call ice_albedo(hsn,t,alb)
+     call ice_surftemp(hice,alb,net_heat,t)
      ice_alb(inod)	 = alb
      ice_temp(inod)      = t+tmelt
 #endif
@@ -423,21 +424,84 @@ contains
  subroutine ice_albedo (hsn,t,alb)
   ! INPUT:
   ! hsn - snow thickness, used for albedo parameterization [m]
+  ! t - temperature of snow/ice surface [C]
+  ! 
+  ! OUTPUT:
+  ! alb - snow albedo
   use i_therm_param
   implicit none
 
   real(kind=WP)  hsn    
   real(kind=WP)  t    
-  real(kind=WP)  alb             ! Albedo of sea ice
+  real(kind=WP)  alb
 
   ! set albedo
   ! ice and snow, freezing and melting conditions are distinguished.
-  if (hsn.gt.0.0) then	!   snow cover present  
-     alb=albsn         	
-  else              		!   no snow cover       
-     alb=albi       	
+  if (t<0.0_WP) then	        ! freezing condition    
+     if (hsn.gt.0.0_WP) then	!   snow cover present  
+        alb=albsn         	
+     else              		!   no snow cover       
+        alb=albi       	
+     endif
+  else			        ! melting condition     
+     if (hsn.gt.0.0_WP) then	!   snow cover present  
+        alb=albsnm	    	
+     else			!   no snow cover       
+        alb=albim		
+     endif
   endif
  end subroutine ice_albedo
+
+
+ subroutine ice_surftemp (hsn,t,alb)
+  ! INPUT:
+  ! A1 - Total atmospheric heat flux
+  ! t - Ice surface temperature (last step)
+  ! 
+  ! OUTPUT:
+  ! alb - snow albedo
+  use i_therm_param
+  implicit none
+
+  !---- atmospheric heat fluxes (provided by OpenIFS)
+  real(kind=WP)  :: a2ohf, a2ihf
+  !---- evaporation and sublimation (provided by OpenIFS)
+  real(kind=WP)  :: evap, subli
+  !---- precipitation and runoff (provided by OpenIFS)
+  real(kind=WP)  :: rain, snow, runo
+  !---- ocean variables (provided by FESOM)
+  real(kind=WP)  :: T_oc, S_oc, ustar
+  real(kind=WP)  hsn
+  real(kind=WP)  t
+  real(kind=WP)  alb
+
+  q1   = 11637800.0_WP
+  q2   = -5897.8_WP
+  imax = 5
+
+  d1=rhoair*cpair*Ch_i
+  d2=rhoair*Ce_i
+  d3=d2*clhi
+
+  ! total incoming atmospheric heat flux
+  A1=a2ihf
+  ! NEWTON-RHAPSON TO GET TEMPERATURE AT THE TOP OF THE ICE LAYER
+
+  do iter=1,imax
+
+     B=q1*inv_rhoair*exp(q2/(t+tmelt))		! (saturated) specific humidity over ice
+     A2=-d1*ug*t-d3*ug*B &
+          -emiss_ice*boltzmann*((t+tmelt)**4)	! sensible and latent heat,and outward radiation
+     A3=-d3*ug*B*q2/((t+tmelt)**2)		! gradient coefficient for the latent heat part
+     C=con/hice                     		! gradient coefficient for downward heat conductivity
+     A3=A3+C+d1*ug & 			! gradient coefficient for sensible heat and radiation 
+          +4.0_WP*emiss_ice*boltzmann*((t+tmelt)**3)    
+     C=C*(TFrez(S_oc)-t)       		! downward conductivity term
+     t=t+(A1+A2+C)/A3 		        ! NEW ICE TEMPERATURE AS THE SUM OF ALL COMPONENTS
+  done
+
+  t=min(0.0_WP,t)
+
 
 end subroutine thermodynamics
 #endif
