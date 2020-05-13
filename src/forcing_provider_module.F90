b4552bc1c85a1fdaf9ebf62c1453e9e2d0f24e56
Jan Hegewald <jan.hegewald@awi.de>
2020-05-13 12:37:37 +0200

    be able to maintain a cache for each forcing data with a fixed lookahead in time

--- src/forcing_provider_module.F90
+++ src/forcing_provider_module.F90
@@ -5,27 +5,94 @@ module forcing_provider_module
   
   type forcing_provider_type
     private
+    type(forcing_reader_type), allocatable :: all_readers(:)
     contains
-    procedure, public :: read_forcingdata
+    procedure, public :: get_forcingdata
   end type
   type(forcing_provider_type), save :: forcing_provider ! handle to singleton instance
   
+  type, private :: forcing_reader_type
+    character(:), allocatable :: basepath
+    character(:), allocatable :: varname
+    integer fileyear
+    integer first_stored_timeindex, last_stored_timeindex
+    real(4), allocatable :: stored_values(:,:,:)
+    integer netcdf_timestep_size
+  end type
+
+  character(len=*), parameter :: FILENAMESUFFIX = ".nc"
+  integer, parameter :: PREFETCH_SIZE = 10
+  
   
   contains
-  subroutine read_forcingdata(this, filepath, varname, t_index1, forcingdata1)
-    class(forcing_provider_type), intent(in) :: this
+
+  subroutine get_forcingdata(this, varindex, filepath, fileyear, varname, time_index, forcingdata)
+    class(forcing_provider_type), intent(inout) :: this
+    integer, intent(in) :: varindex ! todo: remove this arg and just use a hashmap for varname
     character(len=*), intent(in) :: filepath
+    integer, intent(in) :: fileyear
     character(len=*), intent(in) :: varname
-    integer, intent(in) :: t_index1
-    real(8), intent(out) :: forcingdata1(:,:)
+    integer, intent(in) :: time_index
+    real(8), intent(out) :: forcingdata(:,:)
     ! EO args
+    type(forcing_reader_type), allocatable :: tmparr(:)
+    character(:), allocatable :: basepath
+    integer i
+    integer reader_time_index
     real(4), allocatable :: values(:,:,:)
     
-    call read_netcdf_timesteps(filepath, varname, t_index1, t_index1, values)
+    ! init our all_readers array if not already done
+    if(.not. allocated(this%all_readers)) then
+      allocate(this%all_readers(varindex))
+    end if
+        
+    if(size(this%all_readers) < varindex) then
+      allocate( tmparr(varindex) )
+      tmparr(1:size(this%all_readers)) = this%all_readers
+      deallocate(this%all_readers)
+      call move_alloc(tmparr, this%all_readers)
+    end if
+
+    if( len(this%all_readers(varindex)%varname) == 0 ) then ! reader has never been initialized
+      basepath=basepath_from_path(filepath, fileyear)
+      
+      this%all_readers(varindex)%basepath = basepath
+      this%all_readers(varindex)%varname = varname
+      this%all_readers(varindex)%fileyear = fileyear
+      this%all_readers(varindex)%first_stored_timeindex = 0
+      this%all_readers(varindex)%last_stored_timeindex = 0
+      this%all_readers(varindex)%netcdf_timestep_size = read_netcdf_timestep_size(filepath, varname)
+    end if
     
+!    assert(time_index >= reader%first_stored_timeindex) ! we do not go back in time
+    if(this%all_readers(varindex)%last_stored_timeindex < time_index) then
+      
+      this%all_readers(varindex)%first_stored_timeindex = time_index
+      this%all_readers(varindex)%last_stored_timeindex = time_index+PREFETCH_SIZE
+      if(this%all_readers(varindex)%last_stored_timeindex > this%all_readers(varindex)%netcdf_timestep_size) then
+        this%all_readers(varindex)%last_stored_timeindex = this%all_readers(varindex)%netcdf_timestep_size
+      end if
+      call read_netcdf_timesteps(filepath, varname, this%all_readers(varindex)%first_stored_timeindex, this%all_readers(varindex)%last_stored_timeindex, values)
+
+      call assert(allocated(values), __LINE__)
+      if( allocated(this%all_readers(varindex)%stored_values) ) then
+        if(all(shape(values) /= shape(this%all_readers(varindex)%stored_values))) then
+          deallocate(this%all_readers(varindex)%stored_values)
+        end if
+      end if
+      if(.not. allocated(this%all_readers(varindex)%stored_values)) then
+        allocate(this%all_readers(varindex)%stored_values(size(values, DIM=1),size(values, DIM=2),size(values, DIM=3)))
+      end if
+      this%all_readers(varindex)%stored_values = values
+      call assert(allocated(this%all_readers(varindex)%stored_values), __LINE__)
+
+    end if
+  
     ! check if the outgoing array has the same shape as our data
-    call assert( all( shape(forcingdata1)==shape(values(:,:,1)) ), __LINE__ )
-    forcingdata1 = values(:,:,1)
+    reader_time_index = time_index-this%all_readers(varindex)%first_stored_timeindex+1
+    call assert(allocated(this%all_readers(varindex)%stored_values), __LINE__)
+    call assert( all( shape(forcingdata)==shape(this%all_readers(varindex)%stored_values(:,:,reader_time_index)) ), __LINE__ )
+    forcingdata = this%all_readers(varindex)%stored_values(:,:,reader_time_index)    
   end subroutine
   
   
@@ -76,9 +143,9 @@ module forcing_provider_module
     sizes(3) = timeindex_last-timeindex_first+1
     allocate(values(sizes(1),sizes(2),sizes(3)))
     
-    status = nf_get_vara_real(fileid, varid, starts, sizes, values)
-    
+    status = nf_get_vara_real(fileid, varid, starts, sizes, values)    
     status = nf_close(fileid)
+    ! manual deallocation not required, the runtime environment will deallocate the arrays when out of scope
   end subroutine
   
   
