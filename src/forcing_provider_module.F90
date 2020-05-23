9d9a154c3d28048ed968e0dc935ed6f01f170318
Jan Hegewald <jan.hegewald@awi.de>
2020-05-23 17:59:02 +0200

    rename internal forcing_reader_type and move data reading to separate procedure

--- src/forcing_provider_module.F90
+++ src/forcing_provider_module.F90
@@ -6,19 +6,21 @@ module forcing_provider_module
   
   type forcing_provider_type
     private
-    type(forcing_reader_type), allocatable :: all_readers(:)
+    type(forcing_lookahead_reader_type), allocatable :: all_readers(:)
     contains
     procedure, public :: get_forcingdata
   end type
   type(forcing_provider_type), save :: forcing_provider ! handle to singleton instance
   
-  type, private :: forcing_reader_type
+  type, private :: forcing_lookahead_reader_type
     character(:), allocatable :: basepath
     integer fileyear
     integer first_stored_timeindex, last_stored_timeindex
     real(4), allocatable :: stored_values(:,:,:)
     integer netcdf_timestep_size
     type(netcdf_reader_handle) filehandle
+    contains
+    procedure read_data_lookahead
   end type
 
   character(len=*), parameter :: FILENAMESUFFIX = ".nc"
@@ -36,10 +38,9 @@ module forcing_provider_module
     integer, intent(in) :: time_index
     real(4), intent(out) :: forcingdata(:,:)
     ! EO args
-    type(forcing_reader_type), allocatable :: tmparr(:)
+    type(forcing_lookahead_reader_type), allocatable :: tmparr(:)
     character(:), allocatable :: basepath
     integer reader_time_index
-    real(4), allocatable :: values(:,:,:)
     
     ! init our all_readers array if not already done
     if(.not. allocated(this%all_readers)) then
@@ -66,35 +67,46 @@ module forcing_provider_module
       print *,"can not change years", __LINE__, __FILE__
       stop 1
     end if
-    
-!    assert(time_index >= reader%first_stored_timeindex) ! we do not go back in time
-    if(this%all_readers(varindex)%last_stored_timeindex < time_index) then
-      
-      this%all_readers(varindex)%first_stored_timeindex = time_index
-      this%all_readers(varindex)%last_stored_timeindex = time_index+PREFETCH_SIZE
-      if(this%all_readers(varindex)%last_stored_timeindex > this%all_readers(varindex)%netcdf_timestep_size) then
-        this%all_readers(varindex)%last_stored_timeindex = this%all_readers(varindex)%netcdf_timestep_size
-      end if
-      call this%all_readers(varindex)%filehandle%read_netcdf_timesteps(this%all_readers(varindex)%first_stored_timeindex, this%all_readers(varindex)%last_stored_timeindex, values)
 
-      call assert(allocated(values), __LINE__)
-      if( allocated(this%all_readers(varindex)%stored_values) ) then
-        if(all(shape(values) /= shape(this%all_readers(varindex)%stored_values))) then
-          deallocate(this%all_readers(varindex)%stored_values)
-        end if
-      end if
-      if(.not. allocated(this%all_readers(varindex)%stored_values)) then
-        allocate(this%all_readers(varindex)%stored_values(size(values, DIM=1),size(values, DIM=2),size(values, DIM=3)))
-      end if
-      this%all_readers(varindex)%stored_values = values
-      call assert(allocated(this%all_readers(varindex)%stored_values), __LINE__)    
-    end if
+    call this%all_readers(varindex)%read_data_lookahead(time_index)
   
     ! check if the outgoing array has the same shape as our data
     reader_time_index = time_index-this%all_readers(varindex)%first_stored_timeindex+1
     call assert(allocated(this%all_readers(varindex)%stored_values), __LINE__)
     call assert( all( shape(forcingdata)==shape(this%all_readers(varindex)%stored_values(:,:,reader_time_index)) ), __LINE__ )
     forcingdata = this%all_readers(varindex)%stored_values(:,:,reader_time_index)    
+  end subroutine
+  
+  
+  subroutine read_data_lookahead(this, time_index)
+    class(forcing_lookahead_reader_type), intent(inout) :: this
+    integer, intent(in) :: time_index
+    ! EO args
+    real(4), allocatable :: values(:,:,:)
+    
+!    assert(time_index >= reader%first_stored_timeindex) ! we do not go back in time
+    if(this%last_stored_timeindex < time_index) then
+      
+      this%first_stored_timeindex = time_index
+      this%last_stored_timeindex = time_index+PREFETCH_SIZE
+      if(this%last_stored_timeindex > this%netcdf_timestep_size) then
+        this%last_stored_timeindex = this%netcdf_timestep_size
+      end if
+      call this%filehandle%read_netcdf_timesteps(this%first_stored_timeindex, this%last_stored_timeindex, values)
+
+      call assert(allocated(values), __LINE__)
+      if( allocated(this%stored_values) ) then
+        if(all(shape(values) /= shape(this%stored_values))) then
+          deallocate(this%stored_values)
+        end if
+      end if
+      if(.not. allocated(this%stored_values)) then
+        allocate(this%stored_values(size(values, DIM=1),size(values, DIM=2),size(values, DIM=3)))
+      end if
+      this%stored_values = values
+      call assert(allocated(this%stored_values), __LINE__)    
+    end if
+
   end subroutine
   
   
