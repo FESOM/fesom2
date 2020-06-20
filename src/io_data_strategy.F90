42bdbb733f4feb8a8098841c60881db3586ab013
Jan Hegewald <jan.hegewald@awi.de>
2020-06-20 09:19:52 +0200

    create module to manage data type specific I/O implementation

---
+++ src/io_data_strategy.F90
@@ -0,0 +1,83 @@
+module io_data_strategy_module
+  implicit none
+  public data_strategy_type, data_strategy_nf_float_type, data_strategy_nf_double_type, data_strategy_nf_short_type
+  private
+
+  ! there seems to be no way to get this from the netcdf library except a call to nf_inq_type, which requires a netcdf file handle
+  integer, parameter :: nf_float_precision = 4
+  integer, parameter :: nf_double_precision = 8
+  integer, parameter :: nf_short_precision = 2
+
+
+  type, abstract :: data_strategy_type
+  contains
+    procedure(initialize), deferred, nopass :: initialize
+    procedure(netcdf_type), deferred, nopass :: netcdf_type
+  end type
+  interface
+    subroutine initialize()
+    end subroutine
+    
+    function netcdf_type()
+      integer netcdf_type
+    end function
+  end interface
+
+
+  type, extends(data_strategy_type) :: data_strategy_nf_float_type
+  contains
+    procedure, nopass :: initialize => initialize_float
+    procedure, nopass :: netcdf_type => netcdf_type_float
+  end type
+
+
+  type, extends(data_strategy_type) :: data_strategy_nf_double_type
+  contains
+    procedure, nopass :: initialize => initialize_double
+    procedure, nopass :: netcdf_type => netcdf_type_double
+  end type
+
+
+  type, extends(data_strategy_type) :: data_strategy_nf_short_type
+  contains
+    procedure, nopass :: initialize => initialize_short
+    procedure, nopass :: netcdf_type => netcdf_type_short
+  end type
+
+
+contains
+
+
+  subroutine initialize_float()
+  end subroutine
+
+
+  subroutine initialize_double()
+  end subroutine
+
+
+  subroutine initialize_short()
+  end subroutine
+
+
+  function netcdf_type_float()
+    include "netcdf.inc"
+    integer netcdf_type_float
+    netcdf_type_float = nf_float
+  end function
+
+
+  function netcdf_type_double()
+    include "netcdf.inc"
+    integer netcdf_type_double
+    netcdf_type_double = nf_double
+  end function
+
+
+  function netcdf_type_short()
+    include "netcdf.inc"
+    integer netcdf_type_short
+    netcdf_type_short = nf_short
+  end function
+
+end module
