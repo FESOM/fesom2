3f1877c3475cc9458f289079c463338fb8b29f26
Jan Hegewald <jan.hegewald@awi.de>
2020-06-24 17:46:25 +0200

    - remove erroneous if clause in io gather_nod2D - fix erroneous receive buffer allocation

--- src/io_gather.F90
+++ src/io_gather.F90
@@ -18,32 +18,29 @@ contains
     integer        :: req(npes-1)
     integer        :: start, n2D
   
-    if (npes> 1) then
-      call mpi_barrier(MPI_COMM_FESOM,MPIerr)
-      if ( mype == 0 ) then
-        if (npes>1) then
+    if ( mype == 0 ) then
+      if (npes>1) then
     
-        allocate(recvbuf(ubound(arr2D_global,1)))
-          do  n = 1, npes-1
-            n2D   = remPtr_nod2D(n+1) - remPtr_nod2D(n)
-            start = remPtr_nod2D(n)
-            call MPI_IRECV(recvbuf(start), n2D, MPI_DOUBLE_PRECISION, n, 2, MPI_COMM_FESOM, req(n), MPIerr)
-          end do
+        allocate(recvbuf(size(arr2D_global)))
+        do  n = 1, npes-1
+          n2D   = remPtr_nod2D(n+1) - remPtr_nod2D(n)
+          start = remPtr_nod2D(n)
+          call mpi_irecv(recvbuf(start), n2D, MPI_DOUBLE_PRECISION, n, 2, MPI_COMM_FESOM, req(n), MPIerr)
+        end do
 
-          arr2D_global(myList_nod2D(1:myDim_nod2D)) = arr2D(1:myDim_nod2D)
+        arr2D_global(myList_nod2D(1:myDim_nod2D)) = arr2D(1:myDim_nod2D)
 
-          call MPI_WAITALL(npes-1, req, MPI_STATUSES_IGNORE, MPIerr)
+        call mpi_waitall(npes-1, req, MPI_STATUSES_IGNORE, MPIerr)
 
-          arr2D_global(remList_nod2D(1 : remPtr_nod2D(npes)-1)) = recvbuf(1 : remPtr_nod2D(npes)-1)
-          deallocate(recvbuf)
+        arr2D_global(remList_nod2D(1 : remPtr_nod2D(npes)-1)) = recvbuf(1 : remPtr_nod2D(npes)-1)
+        deallocate(recvbuf)
     
-        else
-          arr2D_global(:) = arr2D(:)
-        end if
       else
-  
-        call MPI_SEND( arr2D, myDim_nod2D, MPI_DOUBLE_PRECISION, 0, 2, MPI_COMM_FESOM, MPIerr )
+        arr2D_global(:) = arr2D(:)
       end if
+    else
+  
+      call mpi_send( arr2D, myDim_nod2D, MPI_DOUBLE_PRECISION, 0, 2, MPI_COMM_FESOM, MPIerr )
     end if
   
   end subroutine
