12ce247cbee60d1484779feb6817ca069a5baf4d
Thomas Rackow <thomas.rackow@awi.de>
2020-07-06 18:09:29 +0000

    links successfully on Cray (ECMWF) with the workaround in configure.sh. Set CRAY ON in CMakeLists.txt to prevent an internal error (segmentation fault) of the cray compiler.

--- configure.sh
+++ configure.sh
@@ -6,4 +6,5 @@ source env.sh # source this from your run script too
 mkdir build || true # make sure not to commit this to svn or git
 cd build
 cmake .. # not required when re-compiling
+sed -i -e 's/-lFALSE//g' src/CMakeFiles/fesom.dir/link.txt # workaround for cray
 make install -j`nproc --all`
