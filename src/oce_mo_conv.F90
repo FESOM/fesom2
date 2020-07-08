089469286b707e8db9a37f3339f281d6a603a6f6
Patrick <Patrick.Scholz@awi.de>
2020-07-08 16:44:45 +0200

    correct monin-obukov mixing under cavity

--- src/oce_mo_conv.F90
+++ src/oce_mo_conv.F90
@@ -67,8 +67,9 @@ subroutine mo_convect(mesh)
             if (use_instabmix .and. bvfreq(nz, node) < 0._WP)  Kv(nz,node)=max(Kv(nz,node),instabmix_kv)
             !!PS if (bvfreq(nz, node) < 0._WP)  Kv(nz,node)=kv_conv ! fesom1.4 style
             
-            ! force enhanced wind mixing --> from original fesom1.4 pp mixing
-            if (use_windmix .and. nz<=windmix_nl+1) Kv(nz,node)=max(Kv(nz,node), windmix_kv)
+            if (nzmin>1) cycle
+                ! force enhanced wind mixing --> from original fesom1.4 pp mixing
+                if (use_windmix .and. nz<=windmix_nl+1) Kv(nz,node)=max(Kv(nz,node), windmix_kv)
             
         end do
     end do
@@ -83,15 +84,18 @@ subroutine mo_convect(mesh)
         nzmin = ulevels(elem)
         !!PS do nz=2,nlevels(elem)-1
         do nz=nzmin+1,nzmax-1
-            ! apply monin-obukov mixing to viscosity
-            if (use_momix .and. sum(geo_coord_nod2D(2,elnodes))/3.0<=momix_lat*rad) Av(nz,elem)=Av(nz,elem)+sum(mo(nz,elnodes))/3.0_WP
             
             ! force on convection if unstable 
             if (use_instabmix .and. any(bvfreq(nz, elnodes) < 0._WP)) Av(nz,elem)=max(Av(nz,elem),instabmix_kv)
             !!PS if (any(bvfreq(nz, elnodes) < 0._WP)) Av(nz,elem)=av_conv ! fesom1.4 style
             
-            ! force enhanced wind mixing --> from original fesom1.4 pp mixing
-            if (use_windmix .and. nz<=windmix_nl+1) Av(nz,elem)=max(Av(nz,elem), windmix_kv)
+            ! if cavity dont apply any monin obukov or wind mixing 
+            if (nzmin>1) cycle
+                ! apply monin-obukov mixing to viscosity
+                if (use_momix .and. sum(geo_coord_nod2D(2,elnodes))/3.0<=momix_lat*rad) Av(nz,elem)=Av(nz,elem)+sum(mo(nz,elnodes))/3.0_WP
+                
+                ! force enhanced wind mixing --> from original fesom1.4 pp mixing
+                if (use_windmix .and. nz<=windmix_nl+1) Av(nz,elem)=max(Av(nz,elem), windmix_kv)
         end do
     end do
     !!PS call exchange_elem(Av)
