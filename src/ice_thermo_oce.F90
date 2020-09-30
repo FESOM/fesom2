299c7048ae392bd6be1045d13c2167e53b74b0cc
Jan Streffing <jan.streffing@awi.de>
2020-09-30 21:54:38 +0200

    moving new awicm3 cutoffs into ifdef_oifs p2

--- src/ice_thermo_oce.F90
+++ src/ice_thermo_oce.F90
@@ -18,10 +18,10 @@ where(a_ice<0.1e-8_WP)
 end where
 
 where(m_ice<0.1e-8_WP)
- m_ice=0.0_WP
- m_snow=0.0_WP
- a_ice=0.0_WP 
+ m_ice=0.0_WP 
 #if defined (__oifs)
+ m_snow=0.0_WP
+ a_ice=0.0_WP
  ice_temp=273.15_WP
 #endif /* (__oifs) */
 end where
