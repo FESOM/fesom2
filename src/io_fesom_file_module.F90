2af8a0fb71dc60e6630b8c92ca1af95213a40f25
Jan Hegewald <jan.hegewald@awi.de>
2020-12-10 17:42:30 +0100

    be able to add dimensions to the fesom file type

--- src/io_fesom_file_module.F90
+++ src/io_fesom_file_module.F90
@@ -1,12 +1,20 @@
 module io_fesom_file_module
   implicit none
-  public fesom_file
+  public fesom_file_type
   private
 
 
-  type fesom_file
+  type fesom_file_type
+    private
+    type(dim_type), allocatable :: dims(:)
   contains
-    procedure, public :: initialize
+    procedure, public :: initialize, add_dim
+  end type
+  
+  
+  type dim_type
+    character(:), allocatable :: name
+    integer len
   end type
 
 
@@ -14,8 +22,30 @@ contains
 
 
   subroutine initialize(this)
-    class(fesom_file), intent(inout) :: this
-  end subroutine  
+    class(fesom_file_type), intent(inout) :: this
+  end subroutine
+
+
+  function add_dim(this, name, len) result(dimindex)
+    class(fesom_file_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    integer, intent(in) :: len
+    integer dimindex
+    ! EO parameters
+    type(dim_type), allocatable :: tmparr(:)
     
+    if( .not. allocated(this%dims)) then
+      allocate(this%dims(1))
+    else
+      allocate( tmparr(size(this%dims)+1) )
+      tmparr(1:size(this%dims)) = this%dims
+      deallocate(this%dims)
+      call move_alloc(tmparr, this%dims)
+    end if
+    
+    dimindex = size(this%dims)
+    this%dims(dimindex) = dim_type(name=name, len=len)
+  end function
+  
 
 end module
