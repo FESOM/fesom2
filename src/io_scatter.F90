9b128c5ca0f77e82e6394b5d44d4f857b4b7c798
Jan Hegewald <jan.hegewald@awi.de>
2021-01-12 17:24:02 +0100

    add procedure to distribute elem data to all mesh partitions from any rank

--- src/io_scatter.F90
+++ src/io_scatter.F90
@@ -1,6 +1,6 @@
 module io_scatter_module
   implicit none
-  public scatter_nod2D
+  public scatter_nod2D, scatter_elem2D
   private
   
 contains
@@ -51,6 +51,55 @@ contains
       call mpi_send(mylist_nod2d(1), node_size, mpi_integer, root_rank, tag+1, comm, mpierr)
       
       call mpi_recv(arr2D_local(1), node_size, mpi_precision, root_rank, tag+2, comm, status, mpierr)
+    end if
+  end subroutine
+
+
+  ! thread-safe procedure
+  subroutine scatter_elem2D(arr2D_global, arr2D_local, root_rank, comm)
+    use g_PARSUP
+    use o_mesh
+    use, intrinsic :: iso_fortran_env, only: real64
+    real(real64), intent(in) :: arr2D_global(:)
+    real(real64), intent(out)  :: arr2D_local(:)
+    integer, intent(in) :: root_rank ! rank of sending process
+    integer, intent(in) :: comm
+    ! EO args
+    integer :: tag = 0
+    integer :: mpi_precision = MPI_DOUBLE_PRECISION
+    integer status(MPI_STATUS_SIZE)
+    integer :: n, sender_rank
+    integer, allocatable :: remote_list_elem2d(:)
+    real(real64), allocatable :: sendbuf(:)
+    integer elem_size
+
+    call assert(size(arr2D_local) == size(mylist_elem2d), __LINE__) ! == mydim_elem2d+edim_elem2d, i.e. partition elems + halo elems
+
+    if(mype == root_rank) then
+      arr2D_local = arr2D_global(mylist_elem2d)
+      do  n = 1, npes-1
+        ! receive remote partition 2D size
+        call mpi_recv(elem_size, 1, mpi_integer, MPI_ANY_SOURCE, tag+0, comm, status, mpierr)
+        sender_rank = status(mpi_source)
+
+        ! receive remote mylist_elem2d
+        allocate(remote_list_elem2d(elem_size))
+        call mpi_recv(remote_list_elem2d(1), elem_size, mpi_integer, sender_rank, tag+1, comm, status, mpierr)
+
+        allocate(sendbuf(elem_size))
+        sendbuf = arr2D_global(remote_list_elem2d)
+        deallocate(remote_list_elem2d)
+
+        call mpi_send(sendbuf(1), elem_size, mpi_double_precision, sender_rank, tag+2, comm, mpierr)
+        deallocate(sendbuf)
+      end do
+    
+    else  
+      elem_size = size(mylist_elem2d)
+      call mpi_send(elem_size, 1, mpi_integer, root_rank, tag+0, comm, mpierr)
+      call mpi_send(mylist_elem2d(1), elem_size, mpi_integer, root_rank, tag+1, comm, mpierr)
+      
+      call mpi_recv(arr2D_local(1), elem_size, mpi_precision, root_rank, tag+2, comm, status, mpierr)
     end if
   end subroutine
 
