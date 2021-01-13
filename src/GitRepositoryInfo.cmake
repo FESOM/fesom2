0778b8115be89fcb90c642a64bc24f2145bb1a30
David Guibert <david.guibert@gmail.com>
2021-01-13 11:49:39 +0100

    cmake: don't recompile at each build
    Without this patch, the file `fesom_version_info-generated.F90` is generated
    at each build, rebuilding all its dependencies up to the fesom executable.
    
    The patch generates the fesom_version_info-generated.F90 only when the
    FESOM_GIT_SHA value changes to reflect the current git status at the
    time of building Fesom.
    With this patch it allows to not recompile Fesom if not necessary. For
    example calling make; make (in a row), without any new commits between
    the two make will compile fesom only once. Currently, with this example,
    fesom_version_info-generated.F90 is recompiled because of a new
    timestamp, thus recompiling fesom.

--- src/GitRepositoryInfo.cmake
+++ src/GitRepositoryInfo.cmake
@@ -9,4 +9,18 @@ else()
    message("git not found, setting FESOM_GIT_SHA to: ${FESOM_GIT_SHA}")
 endif()
 
-configure_file(${FESOM_ORIGINAL_VERSION_FILE} ${FESOM_GENERATED_VERSION_FILE} @ONLY)
+set(SHORT_HASH_FILE ${CMAKE_BINARY_DIR}/short_hash.txt)
+if (EXISTS "${SHORT_HASH_FILE}")
+    file(READ ${SHORT_HASH_FILE} READ_IN_SHORT_HASH)
+else()
+    set(READ_IN_SHORT_HASH "")
+endif()
+
+message(STATUS "FESOM_GIT_SHA: '${FESOM_GIT_SHA}'")
+if (NOT ("${READ_IN_SHORT_HASH}" STREQUAL "${FESOM_GIT_SHA}"))
+    message(STATUS "previous FESOM_GIT_SHA: '${READ_IN_SHORT_HASH}' regenerating ${FESOM_GENERATED_VERSION_FILE}")
+    # This will update short_hash.txt, causing cmake to reconfigure
+    file(WRITE ${SHORT_HASH_FILE} ${FESOM_GIT_SHA})
+    configure_file(${FESOM_ORIGINAL_VERSION_FILE} ${FESOM_GENERATED_VERSION_FILE} @ONLY)
+endif()
+
