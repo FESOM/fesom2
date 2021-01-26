80046ecea086b4043c967b0b60aef13bda758d84
patrickscholz <Patrick.Scholz@awi.de>
2021-01-26 14:27:19 +0100

    Update ice_fct.F90
    add another missing cavity limiter

--- src/ice_fct.F90
+++ src/ice_fct.F90
@@ -681,6 +681,10 @@ SUBROUTINE ice_mass_matrix_fill(mesh)
   
     ! TEST: area==sum of row entries in mass_matrix:
     DO q=1,myDim_nod2D
+        !___________________________________________________________________
+        ! if cavity cycle over
+        if(ulevels_nod2d(q)>1) cycle
+        
         !_______________________________________________________________________
         offset=ssh_stiff%rowptr(q)-ssh_stiff%rowptr(1)+1
         n=ssh_stiff%rowptr(q+1)-ssh_stiff%rowptr(1)
