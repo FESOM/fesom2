f464b443e01a166cb44033fa136cdb56f78c46ca
Jan Streffing <jan.streffing@awi.de>
2021-02-12 16:30:20 +0100

    Update ice_thermo_cpl.F90
    enthalpyoffuse hidden behind ifdef oifs

--- src/ice_thermo_cpl.F90
+++ src/ice_thermo_cpl.F90
@@ -30,17 +30,26 @@ subroutine thermodynamics(mesh)
   !---- variables from ice_modules.F90
   use i_dyn_parms,      only: Cd_oce_ice
   use i_therm_parms,    only: rhowat, rhoice, rhosno, cc, cl, con, consn, Sice
+#ifdef oifs
   use i_array,          only: a_ice, m_ice, m_snow, u_ice, v_ice, u_w, v_w  &
        , fresh_wa_flux, net_heat_flux, oce_heat_flux, ice_heat_flux, enthalpyoffuse, S_oc_array, T_oc_array
+#else
+  use i_array,          only: a_ice, m_ice, m_snow, u_ice, v_ice, u_w, v_w  &
+       , fresh_wa_flux, net_heat_flux, oce_heat_flux, ice_heat_flux, S_oc_array, T_oc_array
+#endif
 
   !---- variables from gen_modules_config.F90
   use g_config,         only: dt
 
   !---- variables from gen_modules_forcing.F90
+#ifdef oifs
   use g_forcing_arrays, only: shortwave, evap_no_ifrac, sublimation  &
        , prec_rain, prec_snow, runoff, evaporation, thdgr, thdgrsn, flice  &
        , enthalpyoffuse
-
+#else
+  use g_forcing_arrays, only: shortwave, evap_no_ifrac, sublimation  &
+       , prec_rain, prec_snow, runoff, evaporation, thdgr, thdgrsn, flice
+#endif
   !---- variables from gen_modules_rotate_grid.F90
   use g_rotate_grid,    only: r2g
 #endif
