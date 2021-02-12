9af7ce038ce7924c428519ad541357b842d4f3ad
Jan Streffing <jan.streffing@awi.de>
2021-02-12 16:32:23 +0100

    Update ice_setup_step.F90
    typo

--- src/ice_setup_step.F90
+++ src/ice_setup_step.F90
@@ -132,7 +132,7 @@ e_size=myDim_elem2D+eDim_elem2D
  allocate(stress_iceoce_x(n_size), stress_iceoce_y(n_size))    
  allocate(U_w(n_size), V_w(n_size))   ! =uf and vf of ocean at surface nodes
 #if defined (__oasis)
-  allocate(oce_heat_flux(n_size), ice_heat_flux(n_size)
+  allocate(oce_heat_flux(n_size), ice_heat_flux(n_size))
   allocate(tmp_oce_heat_flux(n_size), tmp_ice_heat_flux(n_size))
 #if defined (__oifs)
   allocate(ice_alb(n_size), ice_temp(n_size), enthalpyoffuse(n_size))
