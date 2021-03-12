067c582f832669a85b00862359d1bac4834dbbb8
Patrick <Patrick.Scholz@awi.de>
2021-03-12 15:59:38 +0100

    exchange area with areasvol where it is neccessary

--- src/gen_support.F90
+++ src/gen_support.F90
@@ -274,6 +274,7 @@ subroutine integrate_nod_2D(data, int2D, mesh)
   do row=1, myDim_nod2D
      !!PS lval=lval+data(row)*area(1,row)
      lval=lval+data(row)*area(ulevels_nod2D(row),row)
+!!PS      lval=lval+data(row)*node_area(row) ! --> TEST_cavity
   end do
 
   int2D=0.0_WP
@@ -300,7 +301,8 @@ subroutine integrate_nod_3D(data, int3D, mesh)
   do row=1, myDim_nod2D
      !!PS do k=1, nlevels_nod2D(row)-1
      do k=ulevels_nod2D(row), nlevels_nod2D(row)-1
-        lval=lval+data(k, row)*area(k,row)*hnode_new(k,row)
+        lval=lval+data(k, row)*area(k,row)*hnode_new(k,row)  ! --> TEST_cavity
+!!PS         lval=lval+data(k, row)*area2(k,row)*hnode_new(k,row)
      end do
   end do
   int3D=0.0_WP
