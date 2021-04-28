0f939813c4f14ad99663f8d4e5b2d8c06746f30b
Dmitry Sidorenko <Dmitry.Sidorenko@awi.de>
2021-04-28 09:43:42 +0200

    modified during Webex meeting with Gregor

--- src/sub_picocpl.F90
+++ src/sub_picocpl.F90
@@ -34,29 +34,35 @@ subroutine init_picocpl(mesh)
   DO ed=1, myDim_edge2D
      if (sum(geo_coord_nod2D(2, edges(:,ed)))/2.>-60.*rad) CYCLE
      if (edge_tri(2,ed)<=0) then
-         el=edge_tri(1, ed)
          coast_Antc(edges(:,ed))=.TRUE.
      end if
   END DO
 
   ! do not read but create the synthetic PICO data
   ! 360 points (longitudes)
+  !!
+  ! read PICO data
+  !!
+  !360
+  !0.   10. 500. -1. 34.
+  !...
+  !359. 10. 500. -1. 34.
   pico_N=360
-  ! PICO data
   allocate(pico_lon(pico_N), pico_z_up(pico_N), pico_z_lo(pico_N), pico_data_t(pico_N), pico_data_s(pico_N))
+  pico_lon= (/(i, i=0, 359)/)
+  ! outflow starts at pico_z_up
+  pico_z_up=  10.
+  ! outflow ends at pico_z_lo
+  pico_z_lo= -500.
+  ! create some data for T & S
+  pico_data_t=-1.0
+  pico_data_s=34.0
+  !end reading PICO
+  !!!
   ! modelled data interpolated onto PICo points
   allocate(f2pico_data_t(pico_N), f2pico_data_s(pico_N), f2pico_count(pico_N))
   ! some temporary array will be required
   allocate(work_array(pico_N))
-  pico_lon= (/(i, i=0, 359)/)
-  ! outflow starts at pico_z_up
-  pico_z_up=   0.
-  ! outflow ends at pico_z_lo
-  pico_z_lo=-500.
-  ! create some data for T & S
-  pico_data_t=-1.0
-  pico_data_s=34.0
-
 end subroutine init_picocpl
 !
 !--------------------------------------------------------------------------------------------
@@ -88,7 +94,7 @@ subroutine fesom2pico(mesh)
         DO k=1, nlevels_nod2D(n)-1
            if ((zbar_3d_n(k,   n) <= pico_z_up(ipico)) .AND. &
                (zbar_3d_n(k+1, n) >= pico_z_lo(ipico))) then
-               f2pico_data_t(ipico)=f2pico_data_t(ipico)+tr_arr(k, n, 1)
+               f2pico_data_t(ipico)=f2pico_data_t(ipico)+tr_arr(k, n, 1) ! here one could do the restoring: (tr_arr(k, n, 1)=tr_arr(k, n, 1)+gamma(pico_data_t-tr_arr(k, n, 1)))
                f2pico_data_s(ipico)=f2pico_data_s(ipico)+tr_arr(k, n, 2)
                f2pico_count (ipico)=f2pico_count (ipico)+1.
            end if
@@ -110,6 +116,10 @@ subroutine fesom2pico(mesh)
   work_array=0.
   call MPI_AllREDUCE(f2pico_count , work_array , pico_N, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_FESOM, MPIerr)
   f2pico_count=work_array
+
+  ! f2pico_data_t<->pico_data_t
+  ! f2pico_data_s<->pico_data_s
+  ! do comparison with PICO & output
   if (mype==0) then
      do n=1, pico_N
         write(*,*) pico_lon(n), nint(f2pico_count(n)), f2pico_data_t(n), f2pico_data_s(n)
