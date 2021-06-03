00b712b7cb9a2463f0e3edbd92e5b56b813ef992
Jan Hegewald <jan.hegewald@awi.de>
2021-06-03 16:32:28 +0200

    add restart_file_group type

--- src/io_restart_file_group.F90
+++ src/io_restart_file_group.F90
@@ -3,8 +3,23 @@ module restart_file_group_module
   implicit none
   private
 
+  
+  type, extends(fesom_file_type) :: restart_file_type
+    private
+    integer iter_varindex
+    character(:), allocatable :: varname
+    character(:), allocatable :: path
+  end type
+
+
+  type restart_file_group
+    private
+    type(restart_file_type) files(20); integer :: nfiles = 0 ! todo: allow dynamically allocated size without messing with shallow copied pointers
+  contains
+  end type
 
 contains
+
 
 
 end module
