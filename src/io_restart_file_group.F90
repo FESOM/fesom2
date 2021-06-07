43375b9617457d9e2f4adee41aa90f2da64931ea
Jan Hegewald <jan.hegewald@awi.de>
2021-06-07 14:22:16 +0200

    be able to access internal variables of the helper restart_file_group type

--- src/io_restart_file_group.F90
+++ src/io_restart_file_group.F90
@@ -1,3 +1,4 @@
+! helper module to treat split restart files similar as the previous single-file ones
 module restart_file_group_module
   use io_fesom_file_module
   implicit none
@@ -6,7 +7,6 @@ module restart_file_group_module
 
   
   type, extends(fesom_file_type) :: restart_file_type
-    private
     integer iter_varindex
     character(:), allocatable :: varname
     character(:), allocatable :: path
@@ -15,7 +15,8 @@ module restart_file_group_module
 
   type restart_file_group
     private
-    type(restart_file_type) files(20); integer :: nfiles = 0 ! todo: allow dynamically allocated size without messing with shallow copied pointers
+    type(restart_file_type), public :: files(20)
+    integer, public :: nfiles = 0 ! todo: allow dynamically allocated size without messing with shallow copied pointers
   contains
     generic, public :: def_node_var => def_node_var_2d, def_node_var_3d
     generic, public :: def_elem_var => def_elem_var_2d, def_elem_var_3d
