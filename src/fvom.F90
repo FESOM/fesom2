6e27468c4b0f121ca785adc3bdd7f6b3487e1a79
Jan Hegewald <jan.hegewald@awi.de>
2021-11-02 10:54:03 +0100

    add modue to save state (e.g. derived types) between calls to fesom_runloop

--- src/fvom.F90
+++ src/fvom.F90
@@ -1,3 +1,12 @@
+! synopsis: save any derived types we initialize
+!           so they can be reused after fesom_init
+module fvom_types_storage_module
+
+end module
+
+! synopsis: main FESOM program split into 3 parts
+!           this way FESOM can e.g. be used as a library with an external time loop driver
+!           used with IFS-FESOM
 module fvom_module
   implicit none
   public fesom_init, fesom_runloop, fesom_finalize
