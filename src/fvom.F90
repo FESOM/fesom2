866db5a8c135401135344e12723a38ba08019e95
Jan Hegewald <jan.hegewald@awi.de>
2021-11-02 11:09:03 +0100

    move FESOM initialization to separate subroutine

--- src/fvom.F90
+++ src/fvom.F90
@@ -15,8 +15,225 @@ module fvom_module
 contains
  
   subroutine fesom_init(nsteps)
+    USE MOD_MESH
+    USE MOD_TRACER
+    USE MOD_PARTIT
+    USE MOD_PARSUP
+    USE o_ARRAYS
+    USE o_PARAM
+    USE i_PARAM
+    use i_ARRAYS
+    use g_clock
+    use g_config
+    use g_comm_auto
+    use g_forcing_arrays
+    use io_RESTART
+    use io_MEANDATA
+    use io_mesh_info
+    use diagnostics
+    use mo_tidal
+    use tracer_init_interface
+    use ocean_setup_interface
+    use ice_setup_interface
+    use ocean2ice_interface
+    use oce_fluxes_interface
+    use update_atm_forcing_interface
+    use before_oce_step_interface
+    use oce_timestep_ale_interface
+    use read_mesh_interface
+    use fesom_version_info_module
+    use command_line_options_module
+    ! Define icepack module
+#if defined (__icepack)
+    use icedrv_main,          only: set_icepack, init_icepack, alloc_icepack
+#endif
+
+#if defined (__oasis)
+    use cpl_driver
+#endif
+
+    implicit none
+
     integer, intent(out) :: nsteps
     ! EO parameters
+
+    integer           :: n, offset, row, i, provided
+    integer, pointer  :: mype, npes, MPIerr, MPI_COMM_FESOM
+    real(kind=WP)     :: t0, t1, t2, t3, t4, t5, t6, t7, t8, t0_ice, t1_ice, t0_frc, t1_frc
+    real(kind=WP)     :: rtime_fullice,    rtime_write_restart, rtime_write_means, rtime_compute_diag, rtime_read_forcing
+    real(kind=real32) :: rtime_setup_mesh, rtime_setup_ocean, rtime_setup_forcing 
+    real(kind=real32) :: rtime_setup_ice,  rtime_setup_other, rtime_setup_restart
+    real(kind=real32) :: mean_rtime(15), max_rtime(15), min_rtime(15)
+    real(kind=real32) :: runtime_alltimesteps
+
+
+    type(t_mesh),       target, save :: mesh
+    type(t_tracer),     target, save :: tracers
+    type(t_partit),     target, save :: partit
+
+
+    character(LEN=256)               :: dump_dir, dump_filename
+    logical                          :: L_EXISTS
+    type(t_mesh),       target, save :: mesh_copy
+    type(t_tracer),     target, save :: tracers_copy
+
+    character(LEN=MPI_MAX_LIBRARY_VERSION_STRING) :: mpi_version_txt
+    integer mpi_version_len
+
+
+      if(command_argument_count() > 0) then
+        call command_line_options%parse()
+        stop
+      end if
+
+#ifndef __oifs
+        !ECHAM6-FESOM2 coupling: cpl_oasis3mct_init is called here in order to avoid circular dependencies between modules (cpl_driver and g_PARSUP)
+        !OIFS-FESOM2 coupling: does not require MPI_INIT here as this is done by OASIS
+        call MPI_INIT_THREAD(MPI_THREAD_MULTIPLE, provided, i)
+#endif
+    
+
+#if defined (__oasis)
+        call cpl_oasis3mct_init(partit%MPI_COMM_FESOM)
+#endif
+        t1 = MPI_Wtime()
+
+        call par_init(partit)
+
+        mype          =>partit%mype
+        MPIerr        =>partit%MPIerr
+        MPI_COMM_FESOM=>partit%MPI_COMM_FESOM
+        npes          =>partit%npes
+        if(mype==0) then
+            write(*,*)
+            print *,"FESOM2 git SHA: "//fesom_git_sha()
+            call MPI_Get_library_version(mpi_version_txt, mpi_version_len, MPIERR)
+            print *,"MPI library version: "//trim(mpi_version_txt)
+            print *, achar(27)//'[32m'  //'____________________________________________________________'//achar(27)//'[0m'
+            print *, achar(27)//'[7;32m'//' --> FESOM BUILDS UP MODEL CONFIGURATION                    '//achar(27)//'[0m'
+        end if
+        !=====================
+        ! Read configuration data,  
+        ! load the mesh and fill in 
+        ! auxiliary mesh arrays
+        !=====================
+        call setup_model(partit)  ! Read Namelists, always before clock_init
+        call clock_init(partit)   ! read the clock file 
+        call get_run_steps(nsteps, partit)
+        call mesh_setup(partit, mesh)
+
+        if (mype==0) write(*,*) 'FESOM mesh_setup... complete'
+    
+        !=====================
+        ! Allocate field variables 
+        ! and additional arrays needed for 
+        ! fancy advection etc.  
+        !=====================
+        call check_mesh_consistency(partit, mesh)
+        if (mype==0) t2=MPI_Wtime()
+
+        call tracer_init(tracers, partit, mesh)                ! allocate array of ocean tracers (derived type "t_tracer")
+        call arrays_init(tracers%num_tracers, partit, mesh)    ! allocate other arrays (to be refactured same as tracers in the future)
+        call ocean_setup(tracers, partit, mesh)
+
+        if (mype==0) then
+           write(*,*) 'FESOM ocean_setup... complete'
+           t3=MPI_Wtime()
+        endif
+        call forcing_setup(partit, mesh)
+
+        if (mype==0) t4=MPI_Wtime()
+        if (use_ice) then 
+            call ice_setup(tracers, partit, mesh)
+            ice_steps_since_upd = ice_ave_steps-1
+            ice_update=.true.
+            if (mype==0) write(*,*) 'EVP scheme option=', whichEVP
+        endif
+        if (mype==0) t5=MPI_Wtime()
+        call compute_diagnostics(0, tracers, partit, mesh) ! allocate arrays for diagnostic
+#if defined (__oasis)
+        call cpl_oasis3mct_define_unstr(partit, mesh)
+        if(mype==0)  write(*,*) 'FESOM ---->     cpl_oasis3mct_define_unstr nsend, nrecv:',nsend, nrecv
+#endif
+
+#if defined (__icepack)
+        !=====================
+        ! Setup icepack
+        !=====================
+        if (mype==0) write(*,*) 'Icepack: reading namelists from namelist.icepack'
+        call set_icepack(partit)
+        call alloc_icepack
+        call init_icepack(tracers%data(1), mesh)
+        if (mype==0) write(*,*) 'Icepack: setup complete'
+#endif
+        call clock_newyear                        ! check if it is a new year
+        if (mype==0) t6=MPI_Wtime()
+        !___CREATE NEW RESTART FILE IF APPLICABLE___________________________________
+        ! The interface to the restart module is made via call restart !
+        ! The inputs are: istep, l_write, l_create
+        ! if istep is not zero it will be decided whether restart shall be written
+        ! if l_write  is TRUE the restart will be forced
+        ! if l_read the restart will be read
+        ! as an example, for reading restart one does: call restart(0, .false., .false., .true., tracers, partit, mesh)
+        call restart(0, .false., r_restart, tracers, partit, mesh) ! istep, l_write, l_read
+        if (mype==0) t7=MPI_Wtime()
+        ! store grid information into netcdf file
+        if (.not. r_restart) call write_mesh_info(partit, mesh)
+
+        !___IF RESTART WITH ZLEVEL OR ZSTAR IS DONE, ALSO THE ACTUAL LEVELS AND ____
+        !___MIDDEPTH LEVELS NEEDS TO BE CALCULATET AT RESTART_______________________
+        if (r_restart) then
+            call restart_thickness_ale(partit, mesh)
+        end if
+        if (mype==0) then
+           t8=MPI_Wtime()
+    
+           rtime_setup_mesh    = real( t2 - t1              ,real32)
+           rtime_setup_ocean   = real( t3 - t2              ,real32)
+           rtime_setup_forcing = real( t4 - t3              ,real32)
+           rtime_setup_ice     = real( t5 - t4              ,real32)
+           rtime_setup_restart = real( t7 - t6              ,real32)
+           rtime_setup_other   = real((t8 - t7) + (t6 - t5) ,real32)
+
+           write(*,*) '=========================================='
+           write(*,*) 'MODEL SETUP took on mype=0 [seconds]      '
+           write(*,*) 'runtime setup total      ',real(t8-t1,real32)      
+           write(*,*) ' > runtime setup mesh    ',rtime_setup_mesh   
+           write(*,*) ' > runtime setup ocean   ',rtime_setup_ocean  
+           write(*,*) ' > runtime setup forcing ',rtime_setup_forcing
+           write(*,*) ' > runtime setup ice     ',rtime_setup_ice    
+           write(*,*) ' > runtime setup restart ',rtime_setup_restart
+           write(*,*) ' > runtime setup other   ',rtime_setup_other 
+            write(*,*) '============================================' 
+        endif
+
+        DUMP_DIR='DUMP/'
+        INQUIRE(file=trim(dump_dir), EXIST=L_EXISTS)
+        if (.not. L_EXISTS) call system('mkdir '//trim(dump_dir))
+
+        write (dump_filename, "(A7,I7.7)") "t_mesh.", mype
+        open  (mype+300, file=TRIM(DUMP_DIR)//trim(dump_filename), status='replace', form="unformatted")
+        write (mype+300) mesh
+        close (mype+300)
+
+    !    open  (mype+300, file=trim(dump_filename), status='old', form="unformatted")
+    !    read  (mype+300) mesh_copy
+    !    close (mype+300)
+         
+        write (dump_filename, "(A9,I7.7)") "t_tracer.", mype
+        open  (mype+300, file=TRIM(DUMP_DIR)//trim(dump_filename), status='replace', form="unformatted")
+        write (mype+300) tracers
+        close (mype+300)
+
+    !    open  (mype+300, file=trim(dump_filename), status='old', form="unformatted")
+    !    read  (mype+300) tracers_copy
+    !    close (mype+300)
+
+    !call par_ex(partit%MPI_COMM_FESOM, partit%mype)
+    !stop
+    !         
+    !    if (mype==10) write(,) mesh1%ssh_stiff%values-mesh%ssh_stiff%value
+
     
   end subroutine
 
