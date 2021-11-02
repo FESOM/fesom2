5e9b757e488da5ef5d8f297eea966ed0a97b5348
Nikolay Koldunov <koldunovn@gmail.com>
2021-11-02 21:16:00 +0100

    add simplest test, plus documentation

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
+cd ../
 
 machine="docker"
 tests="test_pi test_pi_linfs test_pi_zstar test_pi_partial test_pi_floatice test_pi_visc7 test_pi_zstar"
 
 for test in $tests; do
 
-bash -l configure.sh ubuntu
+./configure.sh ubuntu
 echo $test
     mkrun pi $test -m $machine
     pwd
