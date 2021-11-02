566169ee301e1034db8a435580f3583ebc118e13
Nikolay Koldunov <koldunovn@gmail.com>
2021-11-02 15:50:44 +0100

    add more tests and run_tests scripts

---
+++ run_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+machine="docker"
+tests="test_pi test_pi_linfs test_pi_zstar test_pi_partial test_pi_floatice test_pi_visc7 test_pi_zstar"
+
+for test in $tests; do
+
+bash -l configure.sh ubuntu
+echo $test
+    mkrun pi $test -m $machine
+    pwd
+    cd work_pi
+    chmod +x job_docker_new
+    ./job_docker_new
+    fcheck .
+    cd ../
+
+done
+
