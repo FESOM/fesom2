5f23ea907b92983e745db4cb0f7ed7ec1b397700
Patrick Scholz <Patrick.Scholz@awi.de>
2021-11-12 16:31:07 +0100

    add ice derived type initialisation routine into MOD_ICE.F90

--- src/MOD_ICE.F90
+++ src/MOD_ICE.F90
@@ -1,5 +1,5 @@
 MODULE MOD_ICE
-USE I_PARAM
+USE o_PARAM, only: WP
 USE, intrinsic :: ISO_FORTRAN_ENV
 USE MOD_WRITE_BINARY_ARRAYS
 USE MOD_READ_BINARY_ARRAYS
@@ -30,6 +30,9 @@ END TYPE T_ICE_DATA
 TYPE T_ICE_WORK
     !___________________________________________________________________________
     real(kind=WP), allocatable, dimension(:)    :: tmax, tmin
+    real(kind=WP), allocatable, dimension(:)    :: sigma11, sigma12, sigma22
+    real(kind=WP), allocatable, dimension(:)    :: eps11, eps12, eps22
+  
     !___________________________________________________________________________
     contains
         procedure WRITE_T_ICE_WORK
@@ -179,8 +182,14 @@ subroutine WRITE_T_ICE_WORK(twork, unit, iostat, iomsg)
     integer,                intent(in)     :: unit
     integer,                intent(out)    :: iostat
     character(*),           intent(inout)  :: iomsg
-    call write_bin_array(twork%tmin, unit, iostat, iomsg)
-    call write_bin_array(twork%tmax, unit, iostat, iomsg)
+    call write_bin_array(twork%tmin,    unit, iostat, iomsg)
+    call write_bin_array(twork%tmax,    unit, iostat, iomsg)
+    call write_bin_array(twork%sigma11, unit, iostat, iomsg)
+    call write_bin_array(twork%sigma12, unit, iostat, iomsg)
+    call write_bin_array(twork%sigma22, unit, iostat, iomsg)
+    call write_bin_array(twork%eps11,   unit, iostat, iomsg)
+    call write_bin_array(twork%eps12,   unit, iostat, iomsg)
+    call write_bin_array(twork%eps22,   unit, iostat, iomsg)
 end subroutine WRITE_T_ICE_WORK    
 
 ! Unformatted reading for T_ICE_WORK
@@ -192,6 +201,12 @@ subroutine READ_T_ICE_WORK(twork, unit, iostat, iomsg)
     character(*),           intent(inout)  :: iomsg
     call read_bin_array(twork%tmin, unit, iostat, iomsg)
     call read_bin_array(twork%tmax, unit, iostat, iomsg)
+    call read_bin_array(twork%sigma11, unit, iostat, iomsg)
+    call read_bin_array(twork%sigma12, unit, iostat, iomsg)
+    call read_bin_array(twork%sigma22, unit, iostat, iomsg)
+    call read_bin_array(twork%eps11,   unit, iostat, iomsg)
+    call read_bin_array(twork%eps12,   unit, iostat, iomsg)
+    call read_bin_array(twork%eps22,   unit, iostat, iomsg)
 end subroutine READ_T_ICE_WORK
 !
 !
@@ -204,12 +219,12 @@ subroutine WRITE_T_ICE_ATMCOUPL(tcoupl, unit, iostat, iomsg)
     integer,                intent(out)    :: iostat
     character(*),           intent(inout)  :: iomsg
 #if defined (__oasis)    
-    call write_bin_array(tcoupl%oce_flx_h, unit, iostat, iomsg)
-    call write_bin_array(tcoupl%ice_flx_h, unit, iostat, iomsg)
-    call write_bin_array(tcoupl%tmpoce_flx_h, unit, iostat, iomsg)
-    call write_bin_array(tcoupl%tmpice_flx_h, unit, iostat, iomsg)
+    call write_bin_array(tcoupl%oce_flx_h,      unit, iostat, iomsg)
+    call write_bin_array(tcoupl%ice_flx_h,      unit, iostat, iomsg)
+    call write_bin_array(tcoupl%tmpoce_flx_h,   unit, iostat, iomsg)
+    call write_bin_array(tcoupl%tmpice_flx_h,   unit, iostat, iomsg)
 #if defined (__oifs)    
-    call write_bin_array(tcoupl%ice_alb, unit, iostat, iomsg)
+    call write_bin_array(tcoupl%ice_alb,        unit, iostat, iomsg)
     call write_bin_array(tcoupl%enthalpyoffuse, unit, iostat, iomsg)
 #endif /* (__oifs) */
 #endif /* (__oasis) */    
@@ -342,12 +357,186 @@ subroutine READ_T_ICE(ice, unit, iostat, iomsg)
     read(unit, iostat=iostat, iomsg=iomsg) ice%whichEVP
     read(unit, iostat=iostat, iomsg=iomsg) ice%ice_dt
     read(unit, iostat=iostat, iomsg=iomsg) ice%Tevp_inv
-    
 end subroutine READ_T_ICE
-
-
-
-
-
-
 END MODULE MOD_ICE
+!
+!
+!_______________________________________________________________________________
+! interface to initialise derived type for sea ice 
+module ice_init_interface
+    interface
+        subroutine ice_init(ice, partit, mesh)
+        USE MOD_ICE
+        USE MOD_PARSUP
+        USE MOD_PARTIT
+        USE MOD_MESH
+        type(t_ice)   , intent(inout), target :: ice
+        type(t_partit), intent(inout), target :: partit
+        type(t_mesh)  , intent(in)   , target :: mesh
+        end subroutine
+    end interface
+end module
+!
+!
+!_______________________________________________________________________________
+! initialise derived type for sea ice 
+subroutine ice_init(ice, partit, mesh)
+    USE MOD_ICE
+    USE MOD_PARTIT
+    USE MOD_PARSUP
+    USE MOD_MESH
+    USE o_param, only: WP
+    IMPLICIT NONE
+    type(t_ice)   , intent(inout), target :: ice
+    type(t_partit), intent(inout), target :: partit
+    type(t_mesh)  , intent(in)   , target :: mesh
+    !___________________________________________________________________________
+    integer        :: elem_size, node_size, n
+    integer, save  :: nm_unit  = 105       ! unit to open namelist file, skip 100-102 for cray
+    integer        :: iost
+    !___________________________________________________________________________
+    ! define ice namelist parameter
+    integer        :: whichEVP, evp_rheol_steps, ice_ave_steps
+    real(kind=WP)  :: Pstar, ellipse, c_pressure, delta_min, ice_gamma_fct, &
+                      ice_diff, theta_io, alpha_evp, beta_evp, c_aevp, Cd_oce_ice
+    namelist /ice_dyn/ whichEVP, Pstar, ellipse, c_pressure, delta_min, evp_rheol_steps, &
+                       Cd_oce_ice, ice_gamma_fct, ice_diff, theta_io, ice_ave_steps, &
+                       alpha_evp, beta_evp, c_aevp
+    !___________________________________________________________________________
+    ! pointer on necessary derived types
+#include "associate_part_def.h"
+#include "associate_mesh_def.h"
+#include "associate_part_ass.h"
+#include "associate_mesh_ass.h"   
+
+    !___________________________________________________________________________
+    ! open and read namelist.ice for I/O
+        open(unit=nm_unit, file='namelist.ice', form='formatted', access='sequential', status='old', iostat=iost )
+    if (iost == 0) then
+        if (mype==0) write(*,*) '     file   : ', 'namelist.ice',' open ok'
+    else
+        if (mype==0) write(*,*) 'ERROR: --> bad opening file   : ', 'namelist.ice',' ; iostat=',iost
+        call par_ex(partit%MPI_COMM_FESOM, partit%mype)
+        stop
+    end if
+    read(nm_unit, nml=ice_dyn,    iostat=iost)
+    close(nm_unit)
+    
+    !___________________________________________________________________________
+    ! set parameters in ice derived type from namelist.ice
+    ice%whichEVP        = whichEVP 
+    ice%Pstar           = Pstar
+    ice%ellipse         = ellipse
+    ice%c_pressure      = c_pressure
+    ice%delta_min       = delta_min
+    ice%evp_rheol_steps = evp_rheol_steps
+    ice%cd_oce_ice      = Cd_oce_ice
+    ice%ice_gamma_fct   = ice_gamma_fct
+    ice%ice_diff        = ice_diff
+    ice%theta_io        = theta_io
+    ice%ice_ave_steps   = ice_ave_steps
+    ice%alpha_evp       = alpha_evp       
+    ice%beta_evp        = beta_evp
+    ice%c_aevp          = c_aevp
+    
+    !!PS no namelist paramter  in moment 
+    !!PS ice%zeta_min        = zeta_min
+    !!PS ice%Tevp_inv        = Tevp_inv
+    !!PS ice%ice_free_slip   = ice_free_slip
+    !!PS ice%ice_dt          = ice_dt
+    !!PS ice%Tevp_inv        = Tevp_inv
+    
+    !___________________________________________________________________________
+    ! define local vertice & elem array size
+    elem_size=myDim_elem2D+eDim_elem2D
+    node_size=myDim_nod2D+eDim_nod2D
+    
+    !___________________________________________________________________________
+    ! allocate/initialise arrays in ice derived type
+    ! initialise velocity and stress related arrays in ice derived type 
+    allocate(ice%uvice(             2, node_size))
+    allocate(ice%uvice_rhs(         2, node_size))
+    allocate(ice%uvice_old(         2, node_size))
+    allocate(ice%stress_atmice_xy(  2, node_size))
+    allocate(ice%stress_iceoce_xy(  2, node_size))
+    ice%uvice            = 0.0_WP
+    ice%uvice_rhs        = 0.0_WP
+    ice%uvice_old        = 0.0_WP
+    ice%stress_atmice_xy = 0.0_WP
+    ice%stress_iceoce_xy = 0.0_WP
+    
+    !___________________________________________________________________________
+    ! initialise surface ocean arrays in ice derived type 
+    allocate(ice%srfoce_uv(         2, node_size))
+    allocate(ice%srfoce_temp(          node_size))
+    allocate(ice%srfoce_salt(          node_size))
+    allocate(ice%srfoce_ssh(           node_size))
+    ice%srfoce_uv        = 0.0_WP
+    ice%srfoce_temp      = 0.0_WP
+    ice%srfoce_salt      = 0.0_WP
+    ice%srfoce_ssh       = 0.0_WP
+    
+    allocate(ice%flx_fw(node_size))
+    allocate(ice%flx_h( node_size))
+    ice%flx_fw           = 0.0_WP
+    ice%flx_h            = 0.0_WP
+    
+    !___________________________________________________________________________
+    ! initialse data array of ice derived type containing "ice tracer" that have
+    ! to be advected: area (index=1), hice (index=2), hsnow (index=3), 
+    ! ice_temp (index=4, only when coupled)
+    allocate(ice%data(ice%num_itracers))
+    do n = 1, ice%num_itracers
+        allocate(ice%data(n)%values(    node_size))
+        allocate(ice%data(n)%values_old(node_size))
+        allocate(ice%data(n)%values_rhs(node_size))
+        allocate(ice%data(n)%values_div_rhs(node_size))
+        allocate(ice%data(n)%dvalues(   node_size))
+        allocate(ice%data(n)%values1(   node_size))
+        ice%data(n)%ID             = n
+        ice%data(n)%values         = 0.0_WP
+        ice%data(n)%values_old     = 0.0_WP
+        ice%data(n)%values_rhs     = 0.0_WP
+        ice%data(n)%values_div_rhs = 0.0_WP
+        ice%data(n)%dvalues        = 0.0_WP
+        ice%data(n)%values1        = 0.0_WP
+    end do
+    
+    !___________________________________________________________________________
+    ! initialse work array of ice derived type 
+    allocate(ice%work%tmax(            node_size))
+    allocate(ice%work%tmin(            node_size))
+    allocate(ice%work%sigma11(         elem_size))
+    allocate(ice%work%sigma12(         elem_size))
+    allocate(ice%work%sigma22(         elem_size))
+    allocate(ice%work%eps11(           elem_size))
+    allocate(ice%work%eps12(           elem_size))
+    allocate(ice%work%eps22(           elem_size))
+    ice%work%tmax        = 0.0_WP
+    ice%work%tmin        = 0.0_WP
+    ice%work%sigma11     = 0.0_WP
+    ice%work%sigma12     = 0.0_WP
+    ice%work%sigma22     = 0.0_WP
+    ice%work%eps11       = 0.0_WP
+    ice%work%eps12       = 0.0_WP
+    ice%work%eps22       = 0.0_WP
+    
+    !___________________________________________________________________________
+    ! initialse coupling array of ice derived type 
+#if defined (__oasis)    
+    allocate(tcoupl%oce_flx_h          node_size))
+    allocate(tcoupl%ice_flx_h          node_size))
+    allocate(tcoupl%tmpoce_flx_h       node_size))
+    allocate(tcoupl%tmpice_flx_h       node_size))
+    tcoupl%oce_flx_h     = 0.0_WP
+    tcoupl%ice_flx_h     = 0.0_WP
+    tcoupl%tmpoce_flx_h  = 0.0_WP
+    tcoupl%tmpice_flx_h  = 0.0_WP
+#if defined (__oifs)  
+    allocate(tcoupl%ice_alb            node_size))
+    allocate(tcoupl%enthalpyoffuse     node_size))
+    tcoupl%ice_alb       = 0.0_WP
+    tcoupl%enthalpyoffuse= 0.0_WP
+#endif /* (__oifs) */
+#endif /* (__oasis) */        
+end subroutine ice_init    
