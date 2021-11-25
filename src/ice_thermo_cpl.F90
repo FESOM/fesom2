1804cba8c1c26f4026335bb92acbb93b8ff694c8
Patrick <Patrick.Scholz@awi.de>
2021-11-25 13:00:58 +0100

    exchange fresh_wa_flux, net_heat_flux against ice derived type variables in src/ice_thermo_cpl.F90

--- src/ice_thermo_cpl.F90
+++ src/ice_thermo_cpl.F90
@@ -65,21 +65,24 @@ subroutine thermodynamics(ice, partit, mesh)
   real(kind=WP), dimension(:)  , pointer :: a_ice, m_ice, m_snow
   real(kind=WP), dimension(:)  , pointer :: thdgr, thdgrsn
   real(kind=WP), dimension(:)  , pointer :: S_oc_array, T_oc_array, u_w, v_w
+  real(kind=WP), dimension(:)  , pointer :: fresh_wa_flux, net_heat_flux
   myDim_nod2d=>partit%myDim_nod2D
   eDim_nod2D =>partit%eDim_nod2D
   ulevels_nod2D  (1    :myDim_nod2D+eDim_nod2D) => mesh%ulevels_nod2D
   geo_coord_nod2D(1:2,1:myDim_nod2D+eDim_nod2D) => mesh%geo_coord_nod2D
-  u_ice      => ice%uice(:)
-  v_ice      => ice%vice(:)
-  a_ice      => ice%data(1)%values(:)
-  m_ice      => ice%data(2)%values(:)
-  m_snow     => ice%data(3)%values(:)  
-  thdgr      => ice%thermo%thdgr
-  thdgrsn    => ice%thermo%thdgrsn
-  T_oc_array => ice%srfoce_temp(:)
-  S_oc_array => ice%srfoce_salt(:)
-  u_w        => ice%srfoce_u(:)
-  v_w        => ice%srfoce_v(:)
+  u_ice         => ice%uice(:)
+  v_ice         => ice%vice(:)
+  a_ice         => ice%data(1)%values(:)
+  m_ice         => ice%data(2)%values(:)
+  m_snow        => ice%data(3)%values(:)  
+  thdgr         => ice%thermo%thdgr
+  thdgrsn       => ice%thermo%thdgrsn
+  T_oc_array    => ice%srfoce_temp(:)
+  S_oc_array    => ice%srfoce_salt(:)
+  u_w           => ice%srfoce_u(:)
+  v_w           => ice%srfoce_v(:)
+  fresh_wa_flux => ice%flx_fw(:)
+  net_heat_flux => ice%flx_h(:)
   
   !_____________________________________________________________________________  
   rsss = ref_sss
