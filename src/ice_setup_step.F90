2665c4ec3fd77c8bbc210843afd92fdd36e2d708
Patrick <Patrick.Scholz@awi.de>
2021-11-26 16:00:12 +0100

    clean up ice_setup_step.F90

--- src/ice_setup_step.F90
+++ src/ice_setup_step.F90
@@ -1,29 +1,15 @@
-
-module ice_array_setup_interface
-    interface
-        subroutine ice_array_setup(partit, mesh)
-        use mod_mesh
-        USE MOD_PARTIT
-        USE MOD_PARSUP
-        use mod_tracer
-        type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
-        end subroutine
-    end interface
-end module
-
 module ice_initial_state_interface
     interface
         subroutine ice_initial_state(ice, tracers, partit, mesh)
-        use mod_mesh
+        USE MOD_ICE
+        USE MOD_TRACER
         USE MOD_PARTIT
         USE MOD_PARSUP
-        use mod_tracer
-        USE MOD_ICE 
-        type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
-        type(t_tracer), intent(in),    target :: tracers
+        USE MOD_MESH
         type(t_ice)   , intent(inout), target :: ice
+        type(t_tracer), intent(in)   , target :: tracers
+        type(t_partit), intent(inout), target :: partit
+        type(t_mesh)  , intent(in)   , target :: mesh
         end subroutine
     end interface
 end module
@@ -31,15 +17,15 @@ end module
 module ice_setup_interface
     interface
         subroutine ice_setup(ice, tracers, partit, mesh)
-        use mod_mesh
+        USE MOD_ICE
+        USE MOD_TRACER
         USE MOD_PARTIT
         USE MOD_PARSUP
-        use mod_tracer
-        USE MOD_ICE
+        USE MOD_MESH
+        type(t_ice)   , intent(inout), target :: ice
+        type(t_tracer), intent(in)   , target :: tracers
         type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
-        type(t_tracer), intent(in),    target :: tracers
-        type(t_ice),    intent(inout), target :: ice
+        type(t_mesh)  , intent(in)   , target :: mesh
         end subroutine
     end interface
 end module
@@ -47,15 +33,14 @@ end module
 module ice_timestep_interface
     interface
         subroutine ice_timestep(istep, ice, partit, mesh)
-        use mod_mesh
+        USE MOD_ICE
         USE MOD_PARTIT
         USE MOD_PARSUP
-        use mod_tracer
-        USE MOD_ICE
-        integer,        intent(in)            :: istep
+        USE MOD_MESH
+        integer         intent(in)            :: istep
+        type(t_ice)   , intent(inout), target :: ice
         type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
-        type(t_ice),    intent(inout), target :: ice
+        type(t_mesh)  , intent(in)   , target :: mesh
         end subroutine
     end interface
 end module
@@ -64,21 +49,21 @@ end module
 !_______________________________________________________________________________
 ! ice initialization + array allocation + time stepping
 subroutine ice_setup(ice, tracers, partit, mesh)
-    use o_param
-    use g_CONFIG
-    use mod_mesh
+    USE MOD_ICE
+    USE MOD_TRACER
     USE MOD_PARTIT
     USE MOD_PARSUP
-    use mod_tracer
-    use MOD_ICE
+    USE MOD_MESH
+    use o_param
+    use g_CONFIG
     use ice_array_setup_interface
     use ice_initial_state_interface
     use ice_fct_interfaces
     implicit none 
-    type(t_ice),    intent(inout), target :: ice
-    type(t_mesh),   intent(in),    target :: mesh
+    type(t_ice)   , intent(inout), target :: ice
+    type(t_tracer), intent(in)   , target :: tracers
+    type(t_mesh)  , intent(in)   , target :: mesh
     type(t_partit), intent(inout), target :: partit
-    type(t_tracer), intent(in),    target :: tracers
     
     !___________________________________________________________________________
     ! initialise ice derived type 
@@ -90,13 +75,12 @@ subroutine ice_setup(ice, tracers, partit, mesh)
     ice%ice_dt   = real(ice%ice_ave_steps,WP)*dt
     ! ice_dt=dt
     ice%Tevp_inv = 3.0_WP/ice%ice_dt 
-    ice%Clim_evp = ice%Clim_evp*(ice%evp_rheol_steps/ice%ice_dt)**2/ice%Tevp_inv  ! This is combination 
-                                                            ! it always ent
-                                                            
+    ! This is combination it always enters
+    ice%Clim_evp = ice%Clim_evp*(ice%evp_rheol_steps/ice%ice_dt)**2/ice%Tevp_inv  
+    
     !___________________________________________________________________________
     if (flag_debug .and. partit%mype==0)  print *, achar(27)//'[36m'//'     --> call ice_fct_init'//achar(27)//'[0m'
     call ice_mass_matrix_fill(ice, partit, mesh)
-!     call ice_fct_init(ice, partit, mesh)
     
     !___________________________________________________________________________
     ! Initialization routine, user input is required 
@@ -107,136 +91,14 @@ subroutine ice_setup(ice, tracers, partit, mesh)
     if(partit%mype==0) write(*,*) 'Ice is initialized'
 end subroutine ice_setup
 !
-! !
-! !_______________________________________________________________________________
-! subroutine ice_array_setup(partit, mesh)
-! !
-! ! inializing sea ice model 
-! !
-! ! Variables that serve for exchange with atmosphere are nodal, to keep 
-! ! back compatibility with FESOM input routines
-!  
-! use o_param
-! use MOD_MESH
-! USE MOD_PARTIT
-! USE MOD_PARSUP
-! USE g_CONFIG
-! 
-! implicit none
-! type(t_partit), intent(inout), target :: partit
-! type(t_mesh),   intent(in),    target :: mesh
-! integer                               :: n_size, e_size, mn, k, n, n1, n2
-! 
-! #include "associate_part_def.h"
-! #include "associate_mesh_def.h"
-! #include "associate_part_ass.h"
-! #include "associate_mesh_ass.h"
-! 
-! n_size=myDim_nod2D+eDim_nod2D
-! e_size=myDim_elem2D+eDim_elem2D
-! 
-! ! Allocate memory for variables of ice model
-! !  allocate(u_ice(n_size), v_ice(n_size))
-! !  allocate(U_rhs_ice(n_size), V_rhs_ice(n_size))
-! !  allocate(sigma11(e_size), sigma12(e_size), sigma22(e_size))
-! !  allocate(eps11(e_size),     eps12(e_size),   eps22(e_size))
-! !  allocate(m_ice(n_size), a_ice(n_size), m_snow(n_size))
-! !  allocate(rhs_m(n_size), rhs_a(n_size), rhs_ms(n_size))
-! !  allocate(t_skin(n_size))
-! !  allocate(U_ice_old(n_size), V_ice_old(n_size)) !PS
-! !  allocate(m_ice_old(n_size), a_ice_old(n_size), m_snow_old(n_size), thdgr_old(n_size)) !PS
-! !  allocate(thdgr_old(n_size)) !PS
-! !  if (whichEVP > 0) then
-! ! !     allocate(u_ice_aux(n_size), v_ice_aux(n_size))
-! !     allocate(alpha_evp_array(myDim_elem2D))
-! !     allocate(beta_evp_array(n_size))
-! ! 
-! !     alpha_evp_array=alpha_evp
-! !     beta_evp_array =alpha_evp  ! alpha=beta works most reliable
-! ! !     u_ice_aux=0.0_WP
-! ! !     v_ice_aux=0.0_WP
-! !  end if
-!  
-! !  allocate(rhs_mdiv(n_size), rhs_adiv(n_size), rhs_msdiv(n_size))
-! 
-! !  m_ice_old=0.0_WP !PS
-! !  a_ice_old=0.0_WP !PS
-! !  m_snow_old=0.0_WP !PS
-! !  thdgr_old=0.0_WP !PS
-! !  U_ice_old=0.0_WP !PS
-! !  V_ice_old=0.0_WP !PS
-!  
-! !  rhs_m=0.0_WP
-! !  rhs_ms=0.0_WP
-! !  rhs_a=0.0_WP
-! !  m_ice=0.0_WP
-! !  a_ice=0.0_WP
-! !  m_snow=0.0_WP
-! !  U_rhs_ice=0.0_WP
-! !  V_rhs_ice=0.0_WP
-! !  U_ice=0.0_WP
-! !  V_ice=0.0_WP
-! !  sigma11=0.0_WP
-! !  sigma22=0.0_WP
-! !  sigma12=0.0_WP
-! !  eps11=0.0_WP
-! !  eps12=0.0_WP
-! !  eps22=0.0_WP
-! !  t_skin=0.0_WP
-! !  rhs_mdiv=0.0_WP
-! !  rhs_adiv=0.0_WP
-! !  rhs_msdiv=0.0_WP
-! 
-! 
-! ! Allocate memory for arrays used in coupling 
-! ! with ocean and atmosphere
-! !  allocate(S_oc_array(n_size), T_oc_array(n_size))  ! copies of ocean T ans S
-! !  S_oc_array = 0.0_WP
-! !  T_oc_array = 0.0_WP
-! !  allocate(fresh_wa_flux(n_size), net_heat_flux(n_size))
-! !  allocate(fresh_wa_flux(n_size))
-! ! allocate(net_heat_flux(n_size))
-! !  fresh_wa_flux = 0.0_WP
-! !  net_heat_flux = 0.0_WP
-! !  allocate(stress_atmice_x(n_size), stress_atmice_y(n_size))    
-! !  stress_atmice_x = 0.0_WP
-! !  stress_atmice_y = 0.0_WP
-! ! !  allocate(elevation(n_size))           ! =ssh  of ocean        
-! ! !  elevation = 0.0_WP
-! !  allocate(stress_iceoce_x(n_size), stress_iceoce_y(n_size))    
-! !  stress_iceoce_x = 0.0_WP
-! !  stress_iceoce_y = 0.0_WP
-! !  allocate(U_w(n_size), V_w(n_size))   ! =uf and vf of ocean at surface nodes
-! #if defined (__oasis) || defined (__ifsinterface)
-! !   allocate(oce_heat_flux(n_size), ice_heat_flux(n_size))
-! !   allocate(tmp_oce_heat_flux(n_size), tmp_ice_heat_flux(n_size))
-! #if defined (__oifs) || defined (__ifsinterface)
-! !   allocate(ice_alb(n_size), ice_temp(n_size), enthalpyoffuse(n_size))
-! ! allocate(ice_alb(n_size), enthalpyoffuse(n_size))
-! !   allocate(rhs_tempdiv(n_size), rhs_temp(n_size))
-! !   allocate(rhs_tempdiv(n_size))
-! !   ice_alb=0.6_WP
-! !   ice_temp=265.15_WP
-! !   rhs_tempdiv=0._WP
-! !   rhs_temp=0._WP
-! !   enthalpyoffuse=0._WP
-! #endif /* (__oifs) || defined (__ifsinterface) */
-! !   oce_heat_flux=0._WP
-! !   ice_heat_flux=0._WP
-! !   tmp_oce_heat_flux=0._WP
-! !   tmp_ice_heat_flux=0._WP
-! #endif /* (__oasis) || defined (__ifsinterface) */
-! end subroutine ice_array_setup
-! !
-!
 !
 !_______________________________________________________________________________
 ! Sea ice model step
 subroutine ice_timestep(step, ice, partit, mesh)
-    use mod_mesh
+    USE MOD_ICE
     USE MOD_PARTIT
     USE MOD_PARSUP
-    USE MOD_ICE
+    USE MOD_MESH
     use o_param
     use g_CONFIG
     use ice_EVPdynamics_interface
@@ -383,10 +245,10 @@ subroutine ice_initial_state(ice, tracers, partit, mesh)
     use o_arrays        
     use g_CONFIG
     implicit none
-    type(t_ice),    intent(inout), target :: ice
-    type(t_tracer), intent(in),    target :: tracers
+    type(t_ice)   , intent(inout), target :: ice
+    type(t_tracer), intent(in)   , target :: tracers
     type(t_partit), intent(inout), target :: partit
-    type(t_mesh),   intent(in),    target :: mesh
+    type(t_mesh)  , intent(in)   , target :: mesh
     !___________________________________________________________________________
     integer                               :: i
     character(MAX_PATH)                   :: filename
