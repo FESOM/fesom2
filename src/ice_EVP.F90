50662979f95b611dcd83a1d17cd67ff7c086f9bf
Patrick <Patrick.Scholz@awi.de>
2021-11-26 16:42:36 +0100

    clean up src/ice_EVP.F90

--- src/ice_EVP.F90
+++ src/ice_EVP.F90
@@ -5,10 +5,10 @@ module ice_EVP_interfaces
         USE MOD_PARTIT
         USE MOD_PARSUP
         USE MOD_MESH
-        type(t_ice),    intent(inout), target :: ice
+        type(t_ice)   , intent(inout), target :: ice
         type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
-        real(kind=WP),  intent(in)            :: ice_strength(partit%mydim_elem2D)
+        type(t_mesh)  , intent(in)   , target :: mesh
+        real(kind=WP) , intent(in)            :: ice_strength(partit%mydim_elem2D)
         end subroutine
 
         subroutine stress2rhs(inv_areamass, ice_strength, ice, partit, mesh)
@@ -16,10 +16,10 @@ module ice_EVP_interfaces
         USE MOD_PARTIT
         USE MOD_PARSUP
         USE MOD_MESH
-        type(t_ice),    intent(inout), target :: ice
+        type(t_ice)   , intent(inout), target :: ice
         type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
-        real(kind=WP),  intent(in)            :: inv_areamass(partit%myDim_nod2D), ice_strength(partit%mydim_elem2D)
+        type(t_mesh)  , intent(in)   , target :: mesh
+        real(kind=WP) , intent(in)            :: inv_areamass(partit%myDim_nod2D), ice_strength(partit%mydim_elem2D)
         end subroutine
     end interface  
 end module
@@ -31,9 +31,9 @@ module ice_EVPdynamics_interface
         USE MOD_PARTIT
         USE MOD_PARSUP
         USE MOD_MESH
-        type(t_ice),    intent(inout), target :: ice
+        type(t_ice)   , intent(inout), target :: ice
         type(t_partit), intent(inout), target :: partit
-        type(t_mesh),   intent(in),    target :: mesh
+        type(t_mesh)  , intent(in)   , target :: mesh
         end subroutine
     end interface  
 end module
@@ -57,8 +57,8 @@ subroutine stress_tensor(ice_strength, ice, partit, mesh)
 #endif
     implicit none
     type(t_partit), intent(inout), target :: partit
-    type(t_ice),    intent(inout), target :: ice
-    type(t_mesh),   intent(in),    target :: mesh
+    type(t_ice)   , intent(inout), target :: ice
+    type(t_mesh)  , intent(in)   , target :: mesh
     !___________________________________________________________________________
     real(kind=WP), intent(in) :: ice_strength(partit%mydim_elem2D)
     real(kind=WP)   :: eta, xi, delta, aa
@@ -75,14 +75,14 @@ subroutine stress_tensor(ice_strength, ice, partit, mesh)
 #include "associate_mesh_def.h"
 #include "associate_part_ass.h"
 #include "associate_mesh_ass.h"
-    u_ice        => ice%uice(:)
-    v_ice        => ice%vice(:)
-    eps11        => ice%work%eps11(:)
-    eps12        => ice%work%eps12(:)
-    eps22        => ice%work%eps22(:)
-    sigma11      => ice%work%sigma11(:)
-    sigma12      => ice%work%sigma12(:)
-    sigma22      => ice%work%sigma22(:)
+    u_ice   => ice%uice(:)
+    v_ice   => ice%vice(:)
+    eps11   => ice%work%eps11(:)
+    eps12   => ice%work%eps12(:)
+    eps22   => ice%work%eps22(:)
+    sigma11 => ice%work%sigma11(:)
+    sigma12 => ice%work%sigma12(:)
+    sigma22 => ice%work%sigma22(:)
     
     !___________________________________________________________________________
     vale = 1.0_WP/(ice%ellipse**2)
@@ -162,193 +162,6 @@ subroutine stress_tensor(ice_strength, ice, partit, mesh)
         endif
     end do
 end subroutine stress_tensor
-! !===================================================================
-! subroutine stress_tensor_no1(ice_strength, partit, mesh)
-! ! EVP rheology. The routine computes stress tensor components based on ice 
-! ! velocity field. They are stored as elemental arrays (sigma11, sigma22 and
-! ! sigma12). The ocean velocity is at nodal locations.
-! use o_param
-! USE g_CONFIG
-! USE MOD_MESH
-! USE MOD_PARTIT
-! USE MOD_PARSUP
-! implicit none
-! type(t_mesh),   intent(in),    target :: mesh
-! type(t_partit), intent(inout), target :: partit
-! real(kind=WP), intent(in) :: ice_strength(partit%mydim_elem2D)
-! real(kind=WP)   :: eta, xi, delta, aa
-! integer         :: el, elnodes(3)
-! real(kind=WP)   :: asum, msum, vale, dx(3), dy(3)
-! real(kind=WP)   :: det1, det2, r1, r2, r3, si1, si2, dte 
-! real(kind=WP)   :: zeta, delta_inv, d1, d2
-! 
-! #include "associate_part_def.h"
-! #include "associate_mesh_def.h"
-! #include "associate_part_ass.h"
-! #include "associate_mesh_ass.h"
-! 
-!   vale = 1.0_WP/(ice%ellipse**2)
-!    
-!   dte  = ice%ice_dt/(1.0_WP*evp_rheol_steps)
-!   det1 = 1.0_WP/(1.0_WP + 0.5_WP*ice%Tevp_inv*dte)
-!   det2 = 1.0_WP/(1.0_WP + 0.5_WP*ice%Tevp_inv*dte) !*ellipse**2 
-!      
-! 
-!   do el=1,myDim_elem2D
-!      !__________________________________________________________________________
-!      ! if element contains cavity node skip it 
-!      if (ulevels(el) > 1) cycle
-!       ! ===== Check if there is ice on elem
-! 
-!      ! There is no ice in elem 
-!      ! if (any(m_ice(elnodes)<= 0.) .or. any(a_ice(elnodes) <=0.)) CYCLE     
-!      if (ice_strength(el) > 0.) then
-!       ! =====	
-!       ! ===== Deformation rate tensor on element elem:
-!            !du/dx
-! 
-!         eps11(el) = sum(mesh%gradient_sca(1:3,el)*U_ice(mesh%elem2D_nodes(1:3,el))) &
-!                -mesh% metric_factor(el) * sum(V_ice(mesh%elem2D_nodes(1:3,el)))/3.0_WP
-! 
-!         eps22(el) = sum(mesh%gradient_sca(4:6, el)*V_ice(mesh%elem2D_nodes(1:3,el)))
-! 
-!         eps12(el) = 0.5_WP*(sum(mesh%gradient_sca(4:6,el)*U_ice(mesh%elem2D_nodes(1:3,el))) &
-!                       + sum(mesh%gradient_sca(1:3,el)*V_ice(mesh%elem2D_nodes(1:3,el))) &
-!                        + mesh%metric_factor(el) * sum(U_ice(mesh%elem2D_nodes(1:3,el)))/3.0_WP)
-!         ! ===== moduli:
-!         delta = sqrt((eps11(el)*eps11(el) + eps22(el)*eps22(el))*(1.0_WP+vale) + 4.0_WP*vale*eps12(el)*eps12(el) + &
-!                               2.0_WP*eps11(el)*eps22(el)*(1.0_WP-vale))
-! 
-!        ! =======================================
-!        ! ===== Here the EVP rheology piece starts
-!        ! =======================================
-! 
-!       ! ===== viscosity zeta should exceed zeta_min
-!       ! (done via limiting delta from above)
-!       
-!       !if(delta>pressure/ice%zeta_min) delta=pressure/ice%zeta_min
-!            !It does not work properly by 
-! 	   !creating response where ice_strength is small
-!            ! Uncomment and test if necessary
-!       
-!       ! ===== if delta is too small or zero, viscosity will too large (unlimited)
-!       ! (limit delta_inv)
-!         delta_inv = 1.0_WP/max(delta,ice%delta_min)
-!         
-! !!PS         delta_inv = delta/(delta+ice%delta_min)
-!         
-!         zeta = ice_strength(el)*delta_inv			     
-!       ! ===== Limiting pressure/Delta  (zeta): it may still happen that pressure/Delta 
-!       ! is too large in some regions and CFL criterion is violated.
-!       ! The regularization below was introduced by Hunke, 
-!       ! but seemingly is not used in the current CICE. 
-!       ! Without it divergence and zeta can be noisy (but code 
-!       ! remains stable), using it reduces viscosities too strongly.
-!       ! It is therefore commented
-!       
-!       !if (zeta>ice%clim_evp*voltriangle(el)) then
-!       !zeta=ice%clim_evp*voltriangle(el)
-!       !end if 
-!       
-!         zeta = zeta*ice%Tevp_inv
-!         
-!         r1  = zeta*(eps11(el)+eps22(el)) - ice_strength(el)*ice%Tevp_inv
-!         r2  = zeta*(eps11(el)-eps22(el))*vale
-!         r3  = zeta*eps12(el)*vale
-!         
-!         si1 = det1*(sigma11(el) + sigma22(el) + dte*r1)
-!         si2 = det2*(sigma11(el) - sigma22(el) + dte*r2)
-!         
-!         sigma12(el) = det2*(sigma12(el)+dte*r3)
-!         sigma11(el) = 0.5_WP*(si1+si2)
-!         sigma22(el) = 0.5_WP*(si1-si2)
-!      endif
-!   end do
-! end subroutine stress_tensor_no1
-!===================================================================
-! subroutine stress2rhs_e(partit, mesh)
-! ! EVP implementation:
-! ! Computes the divergence of stress tensor and puts the result into the
-! ! rhs vectors. Velocity is at nodes. 
-! ! The divergence is computed in a cysly over edges. It is slower that the
-! ! approach in stress2rhs_e inherited from FESOM
-! USE o_PARAM
-! USE i_therm_param
-! use g_config, only: use_cavity
-! USE MOD_MESH
-! USE MOD_PARTIT
-! USE MOD_PARSUP
-! 
-! IMPLICIT NONE
-! type(t_mesh),   intent(in),    target :: mesh
-! type(t_partit), intent(inout), target :: partit
-! INTEGER       :: n, elem, ed, elnodes(3), el(2), ednodes(2)  
-! REAL(kind=WP) :: mass, uc, vc,  deltaX1, deltaX2, deltaY1, deltaY2
-! #include "associate_part_def.h"
-! #include "associate_mesh_def.h"
-! #include "associate_part_ass.h"
-! #include "associate_mesh_ass.h"
-! 
-!  DO n=1, myDim_nod2D
-!      U_rhs_ice(n)=0.0_WP
-!      V_rhs_ice(n)=0.0_WP
-!  END DO
-!  
-!  ! Stress divergence
-!  DO  ed=1,myDim_edge2D
-!     ednodes=edges(:,ed) 
-!     el=edge_tri(:,ed)
-!     if(myList_edge2D(ed)>edge2D_in) cycle    
-! 
-!     ! stress boundary condition at ocean cavity boundary edge ==0
-!     if (use_cavity) then 
-!         if ( (ulevels(el(1))>1) .or.  ( el(2)>0 .and. ulevels(el(2))>1) ) cycle
-!     end if 
-!     
-!     ! elements on both sides
-!     uc = - sigma12(el(1))*edge_cross_dxdy(1,ed) + sigma11(el(1))*edge_cross_dxdy(2,ed) &
-!          + sigma12(el(2))*edge_cross_dxdy(3,ed) - sigma11(el(2))*edge_cross_dxdy(4,ed)
-!     
-!     vc = - sigma22(el(1))*edge_cross_dxdy(1,ed) + sigma12(el(1))*edge_cross_dxdy(2,ed) &
-!          + sigma22(el(2))*edge_cross_dxdy(3,ed) - sigma12(el(2))*edge_cross_dxdy(4,ed)
-! 
-!     U_rhs_ice(ednodes(1)) = U_rhs_ice(ednodes(1)) + uc
-!     U_rhs_ice(ednodes(2)) = U_rhs_ice(ednodes(2)) - uc
-!     V_rhs_ice(ednodes(1)) = V_rhs_ice(ednodes(1)) + vc
-!     V_rhs_ice(ednodes(2)) = V_rhs_ice(ednodes(2)) - vc
-!  END DO
-!  
-!  DO n=1, myDim_nod2D
-!     !___________________________________________________________________________
-!     ! if cavity node skip it 
-!     if ( ulevels_nod2d(n) > 1 ) cycle
-!     
-!     !___________________________________________________________________________
-!     mass = area(1,n)*(rhoice*m_ice(n)+rhosno*m_snow(n)) 
-!     if(mass > 1.e-3_WP) then 
-!          U_rhs_ice(n) = U_rhs_ice(n) / mass
-!          V_rhs_ice(n) = V_rhs_ice(n) / mass
-!       else
-!          U_rhs_ice(n)=0.0_WP
-!          V_rhs_ice(n)=0.0_WP
-!       end if
-!  END DO
-!  !
-!  ! elevation gradient contribution      
-!  !
-!  do elem=1,myDim_elem2D
-!      !__________________________________________________________________________
-!      ! if element contains cavity node skip it 
-!      if (ulevels(elem) > 1) cycle
-!      
-!      !__________________________________________________________________________
-!      elnodes=elem2D_nodes(:,elem)
-!      uc=elem_area(elem)*g*sum(gradient_sca(1:3,elem)*elevation(elnodes))/3.0_WP
-!      vc=elem_area(elem)*g*sum(gradient_sca(4:6,elem)*elevation(elnodes))/3.0_WP
-!      U_rhs_ice(elnodes)=U_rhs_ice(elnodes) - uc/area(1,elnodes)
-!      V_rhs_ice(elnodes)=V_rhs_ice(elnodes) - vc/area(1,elnodes)
-!  END DO
-! end subroutine stress2rhs_e
 !
 !
 !_______________________________________________________________________________
@@ -363,9 +176,9 @@ subroutine stress2rhs(inv_areamass, ice_strength, ice, partit, mesh)
     USE o_PARAM
     USE i_THERM_PARAM
     IMPLICIT NONE
-    type(t_ice), intent(inout), target :: ice
+    type(t_ice)   , intent(inout), target :: ice
     type(t_partit), intent(inout), target :: partit
-    type(t_mesh),   intent(in),    target :: mesh
+    type(t_mesh)  , intent(in)   , target :: mesh
     !___________________________________________________________________________
     REAL(kind=WP), intent(in) :: inv_areamass(partit%myDim_nod2D), ice_strength(partit%mydim_elem2D)
     INTEGER                   :: n, el,  k
@@ -378,13 +191,13 @@ subroutine stress2rhs(inv_areamass, ice_strength, ice, partit, mesh)
 #include "associate_mesh_def.h"
 #include "associate_part_ass.h"
 #include "associate_mesh_ass.h"
-    sigma11      => ice%work%sigma11(:)
-    sigma12      => ice%work%sigma12(:)
-    sigma22      => ice%work%sigma22(:)
-    u_rhs_ice    => ice%uice_rhs(:)
-    v_rhs_ice    => ice%vice_rhs(:)
-    rhs_a        => ice%data(1)%values_rhs(:)
-    rhs_m        => ice%data(2)%values_rhs(:)
+    sigma11   => ice%work%sigma11(:)
+    sigma12   => ice%work%sigma12(:)
+    sigma22   => ice%work%sigma22(:)
+    u_rhs_ice => ice%uice_rhs(:)
+    v_rhs_ice => ice%vice_rhs(:)
+    rhs_a     => ice%data(1)%values_rhs(:)
+    rhs_m     => ice%data(2)%values_rhs(:)
     
     !___________________________________________________________________________    
     val3=1/3.0_WP
@@ -454,9 +267,9 @@ subroutine EVPdynamics(ice, partit, mesh)
     use icedrv_main,   only: icepack_to_fesom   
 #endif
     IMPLICIT NONE
-    type(t_ice),    intent(inout), target :: ice
+    type(t_ice)   , intent(inout), target :: ice
     type(t_partit), intent(inout), target :: partit
-    type(t_mesh),   intent(in),    target :: mesh
+    type(t_mesh)  , intent(in)   , target :: mesh
     !___________________________________________________________________________
     integer         :: steps, shortstep
     real(kind=WP)   :: rdt, asum, msum, r_a, r_b
@@ -504,7 +317,7 @@ subroutine EVPdynamics(ice, partit, mesh)
     stress_atmice_x => ice%stress_atmice_x(:)
     stress_atmice_y => ice%stress_atmice_y(:)
     
-    !_______________________________________________________________________________
+    !___________________________________________________________________________
     ! If Icepack is used, always update the tracers
 #if defined (__icepack)
     a_ice_old(:)  = a_ice(:)
@@ -516,6 +329,7 @@ subroutine EVPdynamics(ice, partit, mesh)
                             vsno_out=m_snow)
 #endif
 
+    !___________________________________________________________________________
     rdt=ice%ice_dt/(1.0*ice%evp_rheol_steps)
     ax=cos(ice%theta_io)
     ay=sin(ice%theta_io)
@@ -652,8 +466,7 @@ subroutine EVPdynamics(ice, partit, mesh)
         rhs_a(n) = rhs_a(n)/area(1,n)
         rhs_m(n) = rhs_m(n)/area(1,n)
     enddo
-    ! 
-
+    
     !___________________________________________________________________________
     ! End of Precomputing --> And the ice stepping starts
 #if defined (__icepack)
