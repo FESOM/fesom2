e7161a39c96a93a2b9405367e3ac1a0d44d51e9e
Ozgur Gurses <ozgur.gurses@awi.de>
2021-12-17 12:57:35 +0100

    update recom_sinking

--- src/recom_sinking.F90
+++ src/recom_sinking.F90
@@ -29,7 +29,7 @@ subroutine recom_sinking_new(tr_num,mesh)
   Real(kind=8)                      :: wflux(mesh%nl)
   Real(kind=WP)                     :: vd_flux(mesh%nl)
   Real(kind=8)                      :: dz_trr(mesh%nl), aux
-  logical                           :: debug=.false.
+!  logical                           :: debug=.false.
   Real(kind=8)                      :: wLoc,wM,wPs
   Real(kind=8)                      :: Rjp,Rj,Rjm
 
@@ -44,6 +44,8 @@ subroutine recom_sinking_new(tr_num,mesh)
 !< Constant sinking velocities (we prescribe under namelist recom)
 !< This hardcoded part is temporary 
 !< .OG. 07.07.2021
+
+    Vsink=0.0_WP
 
     if (tracer_id(tr_num)==1007 .or. &  !idetn
         tracer_id(tr_num)==1008 .or. &  !idetc
@@ -65,33 +67,39 @@ subroutine recom_sinking_new(tr_num,mesh)
         tracer_id(tr_num)==1015 ) then  !idchl
 
             Vsink = VDia
-    end if 
+    end if
+
+!if (Vsink .lt. 0.1) return ! No sinking if Vsink < 0.1 m/day
 
 if (Vsink .gt. 0.1) then
 
-    do n = 1,myDim_nod2D
-        if (ulevels_nod2D(n)>1) cycle 
-        nzmin = ulevels_nod2D(n)
-        nzmax = nlevels_nod2D(n)-1
+  wflux = 0.d0	
+  dt_sink = dt
+  vd_flux = 0.0d0
 
-        ! distance between tracer points, surface and bottom dz_trr is half 
-        ! the layerthickness
-        dz_trr                = 0.0d0
-        dz_trr(nzmin+1:nzmax) = abs(Z_3d_n(nzmin:nzmax-1,n)-Z_3d_n(nzmin+1:nzmax,n))
-        dz_trr(nzmin)         = hnode(nzmin,n)/2.0d0
-        dz_trr(nzmax+1)       = hnode(nzmax,n)/2.0d0
 
-        Wvel_flux(nzmin:nzmax+1)= 0.d0  ! Vertical velocity for BCG tracers
-                                        ! It can be variable                                        
-        do nz=nzmin,nzmax+1
+  do n = 1,myDim_nod2D
+      if (ulevels_nod2D(n)>1) cycle 
+      nzmin = ulevels_nod2D(n)
+      nzmax = nlevels_nod2D(n)-1
 
-            if (allow_var_sinking) then 
-                Wvel_flux(nz) = -((Vdet_a * abs(zbar_3d_n(nz,n))/SecondsPerDay) + Vsink/SecondsPerDay)
-            else
-                Wvel_flux(nz) = -Vsink/SecondsPerDay
-            end if
-!            if (REcoM_Second_Zoo) then
-! We assume constant sinking for second detritus
+      ! distance between tracer points, surface and bottom dz_trr is half 
+      ! the layerthickness
+      dz_trr                = 0.0d0
+      dz_trr(nzmin+1:nzmax) = abs(Z_3d_n(nzmin:nzmax-1,n)-Z_3d_n(nzmin+1:nzmax,n))
+      dz_trr(nzmin)         = hnode(nzmin,n)/2.0d0
+      dz_trr(nzmax+1)       = hnode(nzmax,n)/2.0d0
+
+      Wvel_flux(nzmin:nzmax+1)= 0.d0  ! Vertical velocity for BCG tracers
+                                                                     
+      do nz=nzmin,nzmax+1
+          if (allow_var_sinking) then 
+             Wvel_flux(nz) = -((Vdet_a * abs(zbar_3d_n(nz,n))/SecondsPerDay) + Vsink/SecondsPerDay)
+          else
+             Wvel_flux(nz) = -Vsink/SecondsPerDay
+          end if
+
+            ! We assume constant sinking for second detritus
             if(tracer_id(tr_num)==1025 .or. &  !idetz2n
                tracer_id(tr_num)==1026 .or. &  !idetz2c
                tracer_id(tr_num)==1027 .or. &  !idetz2si
@@ -99,14 +107,9 @@ if (Vsink .gt. 0.1) then
                Wvel_flux(nz) = -VDet_zoo2/SecondsPerDay ! --> VDet_zoo2
 
             endif
-!            endif
-        end do
+      end do
 
-        wflux = 0.d0	
-        dt_sink = dt
-        vd_flux = 0.0d0
-
-        if (1) then ! 3rd Order DST Sceheme with flux limiting. This code comes from old recom
+if (1) then ! 3rd Order DST Sceheme with flux limiting. This code comes from old recom
 
            k=nod_in_elem2D_num(n)
            ! Screening minimum depth in neigbouring nodes around node n
@@ -143,13 +146,24 @@ if (Vsink .gt. 0.1) then
 	         0.5 * wM  * (tr_arr(max(nzmin,nz-1),n,tr_num) + psiP * Rj))
             vd_flux(nz)= - tv*area(nz,n)
         end do
+
 end if
+
 
 if (0) then ! simple upwind
 
-    vd_flux(nzmin:nzmax+1)= 0.0_WP
+!    vd_flux(nzmin:nzmax+1)= 0.0_WP
 
-    do nz=nzmin+1,nlevels_nod2D_minimum-1
+       vd_flux(nzmin)= 0.0_WP
+       vd_flux(nzmax+1)= 0.0_WP
+
+           k=nod_in_elem2D_num(n)
+           ! Screening minimum depth in neigbouring nodes around node n
+           nlevels_nod2D_minimum=minval(nlevels(nod_in_elem2D(1:k, n))-1)
+
+
+
+    do nz=nzmin+1,nzmax !nlevels_nod2D_minimum-1
 !       tv = tr_arr(nz,n,tr_num)                                ! simple scheme        - test1
 !       tv = 0.5_WP*(tr_arr(nz-1,n,tr_num)+tr_arr(nz,n,tr_num)) ! consider both layers - test2  
 !       tv = tv*Wvel_flux(nz) ! Wvel_flux is negative
@@ -157,31 +171,34 @@ if (0) then ! simple upwind
              (tr_arr(nz-1,n,tr_num)*(Wvel_flux(nz)-abs(Wvel_flux(nz))) + &
               tr_arr(nz  ,n,tr_num)*(Wvel_flux(nz)+abs(Wvel_flux(nz))))
         vd_flux(nz)= tv*area(nz,n)
-    end do
 
+    end do
     ! Every node that touches the ground has zero flux 
-    do nz=nlevels_nod2D_minimum, nzmax
-        tv = - 0.5* &
-             (tr_arr(nz-1,n,tr_num)*(Wvel_flux(nz)-abs(Wvel_flux(nz))) + &
-              tr_arr(nz  ,n,tr_num)*(Wvel_flux(nz)+abs(Wvel_flux(nz))))
-        vd_flux(nz)= tv*(area(nz,n)-area(nz+1,n))*0.d0 ! Test
-    end do
+!    do nz=nlevels_nod2D_minimum, nzmax
+!        tv = - 0.5* &
+!             (tr_arr(nz-1,n,tr_num)*(Wvel_flux(nz)-abs(Wvel_flux(nz))) + &
+!              tr_arr(nz  ,n,tr_num)*(Wvel_flux(nz)+abs(Wvel_flux(nz))))
+!        vd_flux(nz)= tv*(area(nz,n)-area(nz+1,n))*0.d0 ! Test
+!    end do
 
-    nz=nzmax+1
-    tv = - 0.5* &
-         (tr_arr(nz-1,n,tr_num)*(Wvel_flux(nz)-abs(Wvel_flux(nz))) + &
-          tr_arr(nz  ,n,tr_num)*(Wvel_flux(nz)+abs(Wvel_flux(nz))))
-    vd_flux(nz)= tv*area(nz,n)*0.d0 ! Test
+!    nz=nzmax+1
+!    tv = - 0.5* &
+!         (tr_arr(nz-1,n,tr_num)*(Wvel_flux(nz)-abs(Wvel_flux(nz))) + &
+!          tr_arr(nz  ,n,tr_num)*(Wvel_flux(nz)+abs(Wvel_flux(nz))))
+!    vd_flux(nz)= tv*area(nz,n)*0.d0 ! Test
+
 end if 
+
 
 !call integrate_nod(vert_sink(nzmin:nzmax,n), net, mesh)
 !if (mype==0) write(*,*) 'before :', net 
 
 do nz=nzmin,nzmax
-   vert_sink(nz,n) = vert_sink(nz,n) + (vd_flux(nz)-vd_flux(nz+1))*dt_sink/areasvol(nz,n)/(zbar_3d_n(nz,n)-zbar_3d_n(nz+1,n)) ! /dz_trr(nz,n)
+   vert_sink(nz,n) = vert_sink(nz,n) + (vd_flux(nz)-vd_flux(nz+1))*dt/areasvol(nz,n)/hnode_new(nz,n) !*dt_sink/areasvol(nz,n)/(zbar_3d_n(nz,n)-zbar_3d_n(nz+1,n)) ! /dz_trr(nz,n)
 end do
 
-!call integrate_nod(vert_sink, net, mesh)
+!call integrate_nod(vert_sink(nzmin:nzmax,n), net, mesh)
+
 !if (mype==0) write(*,*) 'after :', net 
 
 !call par_ex
