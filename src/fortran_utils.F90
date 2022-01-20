9765c8d427c46afe09bc39c01e04628f4d4bf7db
Jan Hegewald <jan.hegewald@awi.de>
2022-01-20 16:14:24 +0100

    add custom implementation for mkdir

--- src/fortran_utils.F90
+++ src/fortran_utils.F90
@@ -39,5 +39,30 @@ contains
     allocate(character(w) :: txt)
     write(txt,'(i0.'//widthtxt//')') val
   end function
+  
+  
+  ! using EXECUTE_COMMAND_LINE to call mkdir sometimes fail (EXECUTE_COMMAND_LINE is forked as an new process, which may be the problem)
+  ! try to use the C mkdir as an alternative
+  subroutine mkdir(path)
+    use iso_c_binding
+    character(len=*), intent(in) :: path
+    ! EO parameters
+    integer result
+    character(:,kind=C_CHAR), allocatable :: pathcopy
+
+    interface
+      function mkdir_c(path, mode) bind(c,name="mkdir")
+        use iso_c_binding
+        integer(c_int) mkdir_c
+        character(kind=c_char,len=1) path(*)
+        integer(c_int), value :: mode
+      end function
+    end interface
+      
+    pathcopy = path ! we need to pass an array of c_char to the C funcktion (this is not a correct type conversion, but Fortran characters seem to be of the same kind as c_char)
+    ! result is 0 if the dir has been created from this call, otherwise -1
+    ! the mode will not exactly be what we pass here, as it is subtracted by the umask bits (and possibly more)
+    result = mkdir_c(pathcopy//C_NULL_CHAR, int(o'777', c_int))
+  end subroutine
 
 end module
