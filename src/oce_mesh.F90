74b800723ecf6148c5c3d0b3d1e859568c7b0fea
JanStreffing <jan.streffing@awi.de>
2022-01-26 18:01:01 +0100

    Revert "remove impact of earth curvature on u and v derivatives"
    This reverts commit 96e8da16dbcd58083d91c355e4c2bc9935394a59.

--- src/oce_mesh.F90
+++ src/oce_mesh.F90
@@ -2021,7 +2021,7 @@ t0=MPI_Wtime()
  center_x(n)=ax
  center_y(n)=ay
  mesh%elem_cos(n)=cos(ay)
- mesh%metric_factor=0.0
+ mesh%metric_factor=tan(ay)/r_earth
  END DO
 
  call exchange_elem(mesh%metric_factor)
