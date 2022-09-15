df9c43c38636b5663d3964fcf8e7b40dbaa9443b
tsemmler05 <tido.semmler@gmail.com>
2022-09-15 16:37:53 +0900

    add transport calculation

--- src/gen_modules_diag.F90
+++ src/gen_modules_diag.F90
@@ -17,7 +17,7 @@ module diagnostics
 !!PS   
   public :: ldiag_solver, lcurt_stress_surf, ldiag_energy, ldiag_dMOC, ldiag_DVD, ldiag_forc, ldiag_salt3D, ldiag_curl_vel3, diag_list, &
             compute_diagnostics, rhs_diag, curl_stress_surf, curl_vel3, wrhof, rhof, &
-            u_x_u, u_x_v, v_x_v, v_x_w, u_x_w, dudx, dudy, dvdx, dvdy, dudz, dvdz, utau_surf, utau_bott, av_dudz_sq, av_dudz, av_dvdz, stress_bott, u_surf, v_surf, u_bott, v_bott, &
+            u_x_u, u_x_v, v_x_v, v_x_w, u_x_w, T_x_u, T_x_v, T_x_w, S_x_u, S_x_v, S_x_w, dudx, dudy, dvdx, dvdy, dudz, dvdz, utau_surf, utau_bott, av_dudz_sq, av_dudz, av_dvdz, stress_bott, u_surf, v_surf, u_bott, v_bott, &
             std_dens_min, std_dens_max, std_dens_N, std_dens, std_dens_UVDZ, std_dens_DIV, std_dens_Z, std_dens_dVdT, std_dens_flux, dens_flux, &
             compute_diag_dvd_2ndmoment_klingbeil_etal_2014, compute_diag_dvd_2ndmoment_burchard_etal_2008, compute_diag_dvd
   ! Arrays used for diagnostics, some shall be accessible to the I/O
@@ -229,7 +229,7 @@ subroutine diag_energy(mode, mesh)
 !=====================
   if (firstcall) then  !allocate the stuff at the first call
      allocate(wrhof(nl, myDim_nod2D), rhof(nl, myDim_nod2D))
-     allocate(u_x_u(nl-1, myDim_nod2D), u_x_v(nl-1, myDim_nod2D), v_x_v(nl-1, myDim_nod2D), v_x_w(nl-1, myDim_elem2D), u_x_w(nl-1, myDim_elem2D))
+     allocate(u_x_u(nl-1, myDim_nod2D), u_x_v(nl-1, myDim_nod2D), v_x_v(nl-1, myDim_nod2D), v_x_w(nl-1, myDim_elem2D), u_x_w(nl-1, myDim_elem2D), T_x_u(nl-1, myDim_nod2D), T_x_v(nl-1, myDim_nod2D), T_x_w(nl-1, myDim_elem2D), S_x_u(nl-1, myDim_nod2D), S_x_v(nl-1, myDim_nod2D), S_x_w(nl-1, myDim_elem2D))
      allocate(dudx(nl-1, myDim_nod2D), dudy(nl-1, myDim_nod2D), dvdx(nl-1, myDim_nod2D), dvdy(nl-1, myDim_nod2D), dudz(nl, myDim_elem2D), dvdz(nl, myDim_elem2D))
      allocate(utau_surf(myDim_elem2D), utau_bott(myDim_elem2D), av_dudz_sq(nl, myDim_elem2D), av_dudz(nl, myDim_elem2D), av_dvdz(nl, myDim_elem2D))
      allocate(u_surf(myDim_elem2D), v_surf(myDim_elem2D), u_bott(myDim_elem2D), v_bott(myDim_elem2D), stress_bott(2, myDim_elem2D))
@@ -240,6 +240,12 @@ subroutine diag_energy(mode, mesh)
      v_x_v=0.
      u_x_w=0.
      v_x_w=0.
+     T_x_u=0.
+     T_x_v=0.
+     T_x_w=0.
+     S_x_u=0.
+     S_x_v=0.
+     S_x_w=0.
      dudx=0.
      dudy=0.
      dvdx=0.
@@ -268,6 +274,10 @@ subroutine diag_energy(mode, mesh)
   u_x_u=Unode(1,1:nl-1,1:myDim_nod2D)*Unode(1,1:nl-1,1:myDim_nod2D)
   u_x_v=Unode(1,1:nl-1,1:myDim_nod2D)*Unode(2,1:nl-1,1:myDim_nod2D)
   v_x_v=Unode(2,1:nl-1,1:myDim_nod2D)*Unode(2,1:nl-1,1:myDim_nod2D)
+  T_x_u=Unode(1,1:nl-1,1:myDim_nod2D)*tracers...(1 for temperature, 2 for salinity)
+  T_x_v=
+  S_x_u=
+  S_x_v=
   ! this loop might be very expensive
   DO n=1, myDim_elem2D
      nzmax = nlevels(n)
@@ -357,6 +367,7 @@ subroutine diag_energy(mode, mesh)
      !!PS wrhof(1:nzmax, n) = rhof(1:nzmax, n)*Wvel(1:nzmax, n)
      zbar_n(nzmin)         = zbar_n(nzmin+1) + hnode_new(nzmin,n)
      wrhof(nzmin:nzmax, n) = rhof(nzmin:nzmax, n)*Wvel(nzmin:nzmax, n)
+     T_x_w .... = tracers .... * Wvel(nzmin:nzmax, n)
 
      !!PS DO nz=1, nzmax-1
      DO nz=nzmin, nzmax-1
