ece1c103730ebd45de972a79c6f34a4b7439d70f
Jan Hegewald <jan.hegewald@awi.de>
2022-11-08 14:59:53 +0100

    add ENABLE_NVHPC_WORKAROUNDS macro to the info module

--- src/info_module.F90
+++ src/info_module.F90
@@ -97,6 +97,11 @@ contains
 #else
       print '(g0)', 'ENABLE_ALEPH_CRAYMPICH_WORKAROUNDS is OFF'
 #endif  
+#ifdef ENABLE_NVHPC_WORKAROUNDS
+      print '(g0)', 'ENABLE_NVHPC_WORKAROUNDS is ON'
+#else
+      print '(g0)', 'ENABLE_NVHPC_WORKAROUNDS is OFF'
+#endif  
   end subroutine
 
 end module
