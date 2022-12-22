2d5f8549b447696ba2cffe60292f300dfbd2ed68
WiltonLoch <wilton.loch@gmail.com>
2022-12-22 14:44:13 +0100

    porting(advection): collapsing atomics kernel loop in oce_tra_adv_fct subroutine

--- src/oce_adv_tra_fct.F90
+++ src/oce_adv_tra_fct.F90
@@ -266,65 +266,64 @@ subroutine oce_tra_adv_fct(dt, ttf, lo, adf_h, adf_v, fct_ttf_min, fct_ttf_max, 
 !$OMP DO
     !Horizontal
 #if !defined(DISABLE_OPENACC_ATOMICS)
-    !$ACC PARALLEL LOOP GANG PRIVATE(enodes, el) DEFAULT(PRESENT) VECTOR_LENGTH(acc_vl)
+    !$ACC PARALLEL LOOP GANG VECTOR COLLAPSE(2) PRIVATE(enodes, el) DEFAULT(PRESENT) VECTOR_LENGTH(acc_vl)
 #else
     !$ACC UPDATE SELF(fct_plus, fct_minus, adf_h)
 #endif
     do edge=1, myDim_edge2D
-       enodes(1:2)=edges(:,edge)
-       el=edge_tri(:,edge)
-       nl1=nlevels(el(1))-1
-       nu1=ulevels(el(1))
-       nl2=0
-       nu2=0
-       if (el(2)>0) then
-          nl2=nlevels(el(2))-1
-          nu2=ulevels(el(2))
-       end if
+        do nz=1, nl
+            enodes(1:2)=edges(:,edge)
+            el=edge_tri(:,edge)
+            nl1=nlevels(el(1))-1
+            nu1=ulevels(el(1))
+            nl2=0
+            nu2=0
+            if (el(2)>0) then
+                nl2=nlevels(el(2))-1
+                nu2=ulevels(el(2))
+            end if
 
-       nl12 = max(nl1,nl2)
-       nu12 = nu1
-       if (nu2>0) nu12 = min(nu1,nu2)
+            nl12 = max(nl1,nl2)
+            nu12 = nu1
+            if (nu2>0) nu12 = min(nu1,nu2)
 #if defined(_OPENMP)  && !defined(__openmp_reproducible)
-       call omp_set_lock(partit%plock(enodes(1)))
+           call omp_set_lock(partit%plock(enodes(1)))
 #else
 !$OMP ORDERED
 #endif
+            if(nu12 <= nz .and. nz <= nl12) then
+                ! do nz=nu12, nl12
 #if !defined(DISABLE_OPENACC_ATOMICS)
-       !$ACC LOOP VECTOR
+                !$ACC ATOMIC UPDATE
 #endif
-       do nz=nu12, nl12
+                fct_plus (nz,enodes(1))=fct_plus (nz,enodes(1)) + max(0.0_WP, adf_h(nz,edge))
 #if !defined(DISABLE_OPENACC_ATOMICS)
-          !$ACC ATOMIC UPDATE
+                !$ACC ATOMIC UPDATE
 #endif
-          fct_plus (nz,enodes(1))=fct_plus (nz,enodes(1)) + max(0.0_WP, adf_h(nz,edge))
-#if !defined(DISABLE_OPENACC_ATOMICS)
-          !$ACC ATOMIC UPDATE
-#endif
-          fct_minus(nz,enodes(1))=fct_minus(nz,enodes(1)) + min(0.0_WP, adf_h(nz,edge))
+                fct_minus(nz,enodes(1))=fct_minus(nz,enodes(1)) + min(0.0_WP, adf_h(nz,edge))
 #if defined(_OPENMP)  && !defined(__openmp_reproducible)
-       end do
-       call omp_unset_lock(partit%plock(enodes(1)))
-       call omp_set_lock  (partit%plock(enodes(2)))
-       do nz=nu12, nl12
+                ! end do
+                call omp_unset_lock(partit%plock(enodes(1)))
+                call omp_set_lock  (partit%plock(enodes(2)))
+                ! do nz=nu12, nl12
 #endif
 #if !defined(DISABLE_OPENACC_ATOMICS)
-          !$ACC ATOMIC UPDATE
+                !$ACC ATOMIC UPDATE
 #endif
-          fct_plus (nz,enodes(2))=fct_plus (nz,enodes(2)) + max(0.0_WP,-adf_h(nz,edge))
+                fct_plus (nz,enodes(2))=fct_plus (nz,enodes(2)) + max(0.0_WP,-adf_h(nz,edge))
 #if !defined(DISABLE_OPENACC_ATOMICS)
-          !$ACC ATOMIC UPDATE
+                !$ACC ATOMIC UPDATE
 #endif
-          fct_minus(nz,enodes(2))=fct_minus(nz,enodes(2)) + min(0.0_WP,-adf_h(nz,edge))
-       end do
-#if !defined(DISABLE_OPENACC_ATOMICS)
-       !$ACC END LOOP
-#endif
+                fct_minus(nz,enodes(2))=fct_minus(nz,enodes(2)) + min(0.0_WP,-adf_h(nz,edge))
+                ! end do
 #if defined(_OPENMP)  && !defined(__openmp_reproducible)
-       call omp_unset_lock(partit%plock(enodes(2)))
+               call omp_unset_lock(partit%plock(enodes(2)))
+            end if
 #else
+            end if
 !$OMP END ORDERED
 #endif
+        end do
     end do
 #if !defined(DISABLE_OPENACC_ATOMICS)
     !$ACC END PARALLEL LOOP
