ee03398b6cebb003594de24754bdc27c11024652
Ozgur Gurses <ozgur.gurses@awi.de>
2023-03-20 09:50:43 +0100

    Environment and cmake settings for Albedo

--- env.sh
+++ env.sh
@@ -29,6 +29,8 @@ if [[ $LOGINHOST =~ ^m[A-Za-z0-9]+\.hpc\.dkrz\.de$ ]]; then
    STRATEGY="mistral.dkrz.de"
 elif [[ $LOGINHOST =~ ^ollie[0-9]$ ]] || [[ $LOGINHOST =~ ^prod-[0-9]{4}$ ]]; then
    STRATEGY="ollie"
+elif [[ $LOGINHOST =~ ^albedo[0-9]$ ]] || [[ $LOGINHOST =~ ^prod-[0-9]{4}$ ]]; then
+   STRATEGY="albedo"
 elif [[ $LOGINHOST =~ ^h[A-Za-z0-9]+\.hsn\.hlrn\.de$ ]]; then
    STRATEGY="hlogin.hlrn.de"
 elif [[ $LOGINHOST =~ ^b[A-Za-z0-9]+\.usr\.hlrn\.de$ ]]; then
