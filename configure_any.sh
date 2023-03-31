ba7e9ac3b778757155d93804fc710933da162873
Thomas Rackow <thomas.rackow@ecmwf.int>
2023-03-31 10:15:58 +0200

    Update configure_any.sh
    Remove platform dependence in configure_any.sh

--- configure_any.sh
+++ configure_any.sh
@@ -18,7 +18,7 @@ source env.sh # source this from your run script too
 if [[ ${LIB} = yes ]]; then
   mkdir build.lib || true # build dir for library
   cd build.lib
-  cmake -DBUILD_FESOM_AS_LIBRARY=ON -DOPENMP_REPRODUCIBLE=OFF -DFESOM_INSTALL_PREFIX=/work/bm1235/${USER}/ifs-bundle-c3/fesom2.5/ -DBUILD_SHARED_LIBS=ON -DDISABLE_MULTITHREADING=ON -DENABLE_OPENMP=ON .. # not required when re-compiling
+  cmake -DBUILD_FESOM_AS_LIBRARY=ON -DOPENMP_REPRODUCIBLE=OFF -DFESOM_INSTALL_PREFIX=$PWD -DBUILD_SHARED_LIBS=ON -DDISABLE_MULTITHREADING=ON -DENABLE_OPENMP=ON .. # not required when re-compiling
   sed -i -e 's/-lFALSE//g' src/CMakeFiles/fesom.dir/link.txt # workaround for the moment on cray
 else
   mkdir build || true # build dir for binary
