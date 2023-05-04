ca86fd6e89658358d735465b3e530cc962164e29
Patrick <Patrick.Scholz@awi.de>
2023-05-04 16:43:33 +0200

    implementing warning when opt_visc=5 is used in an eddy resolving to eddy permitting configuration

--- src/MOD_DYN.F90
+++ src/MOD_DYN.F90
@@ -76,6 +76,7 @@ TYPE T_DYN
     ! 6=Biharmonic flow aware (viscosity depends on velocity Laplacian)
     ! 7=Biharmonic flow aware (viscosity depends on velocity differences)
     ! 8=Dynamic Backscatter
+    integer                                     :: check_opt_visc= .true.
     integer                                     :: opt_visc      = 5
 
     ! gamma0 [m/s],   backgroung viscosity= gamma0*len, it should be as small
--- src/oce_ale.F90
+++ src/oce_ale.F90
@@ -2919,6 +2919,7 @@ subroutine oce_timestep_ale(n, ice, dynamics, tracers, partit, mesh)
     use write_step_info_interface
     use check_blowup_interface
     use fer_solve_interface
+    use check_validviscopt_interface
     IMPLICIT NONE
     integer       , intent(in)            :: n
     type(t_dyn)   , intent(inout), target :: dynamics
@@ -2959,7 +2960,13 @@ subroutine oce_timestep_ale(n, ice, dynamics, tracers, partit, mesh)
     else  
         call pressure_force_4_zxxxx(tracers, partit, mesh)
     end if
-
+    
+    !___________________________________________________________________________
+    ! check validity of visc_opt=5 selection
+    ! --> need to know buoyancy frequency to do so.
+    ! --> only check on the first timestep 
+    if (n==1) call check_viscopt(dynamics, partit, mesh)
+    
     !___________________________________________________________________________
     ! calculate alpha and beta
     ! it will be used for KPP, Redi, GM etc. Shall we keep it on in general case?
