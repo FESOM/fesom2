29bfe1940e98c91dd56c3e3f5c25cee99777f1ed
Nils-Arne Dreier <dreier@dkrz.de>
2023-05-17 16:06:29 +0200

    switch to volume grid for yac

--- src/cpl_yac_driver.F90
+++ src/cpl_yac_driver.F90
@@ -9,6 +9,7 @@ module cpl_yac_driver
   character(len=*), PARAMETER   :: comp_name = "fesom2"
   integer :: comp_id, grid_id, points_id
   integer :: send_field_id(4), recv_field_id(12)
+  real(kind=WP) :: delta
   real(kind=WP), dimension(:,:),   allocatable   :: a2o_fcorr_stat  !flux correction statistics for the output
   integer, parameter         :: nsend = 4
   integer, parameter         :: nrecv = 12
@@ -54,58 +55,139 @@ contains
 
     type(t_mesh),   intent(in),    target :: mesh
     type(t_partit), intent(inout), target :: partit
-    integer :: ierr, elnodes(3), elem, k, i, j, dimsize, nbr_connections
-    real(kind=WP) :: ax(3), amin, x, y
-    real(kind=WP), allocatable :: elem_x(:), elem_y(:)
+    real(kind=WP), allocatable :: x_vertices(:), y_vertices(:)
+    integer, allocatable :: nbr_vertices_per_cell(:), cell_to_vertex(:)
+    integer :: ierr, i, j, k, nbr_vertices, nbr_boundary_nodes, nbr_connections, vtx_idx, c2v_idx
+    integer :: curr_elem, curr_edge
+    logical, allocatable :: node_is_boundary(:)
     character(len=4)           :: dt_str
-    integer, allocatable :: cell_to_vertex(:)
 
 #include "associate_part_def.h"
 #include "associate_mesh_def.h"
 #include "associate_part_ass.h"
 #include "associate_mesh_ass.h"
 
-    allocate(elem_x(myDim_elem2D))
-    allocate(elem_y(myDim_elem2D))
-    DO elem=1,myDim_elem2D
-       elnodes=mesh%elem2D_nodes(:,elem)
-       ax=mesh%coord_nod2D(1, elnodes)
-       amin=minval(ax)
-       DO k=1,3
-          if(ax(k)-amin>=cyclic_length/2.0_WP) ax(k)=ax(k)-cyclic_length
-          if(ax(k)-amin<-cyclic_length/2.0_WP) ax(k)=ax(k)+cyclic_length
-       END DO
-       x=sum(ax)/3.0_WP
-       y=sum(mesh%coord_nod2D(2,elnodes))/3.0_WP
-       CALL r2g(elem_x(elem), elem_y(elem), x, y)
+    ! find boundary nodes
+    ALLOCATE(node_is_boundary(myDim_nod2D))
+    node_is_boundary = .FALSE.
+    DO i=1,myDim_edge2D
+       IF (myList_edge2D(i) > edge2D_in) THEN
+          node_is_boundary(edges(1,i)) = .TRUE.
+          node_is_boundary(edges(2,i)) = .TRUE.
+       END IF
     END DO
+    nbr_boundary_nodes = COUNT(node_is_boundary)
 
-    nbr_connections = SUM(nod_in_elem2D_num(1:myDim_nod2D))
+    nbr_vertices = myDim_elem2D + myDim_edge2D + nbr_boundary_nodes
+    nbr_connections = 2*SUM(nod_in_elem2D_num(1:myDim_nod2D)) + 2*nbr_boundary_nodes
 
+    WRITE (0,*) "nbr_boundary_nodes: ", nbr_boundary_nodes
+    WRITE (0,*) "nbr_vertices: " , nbr_vertices
+    WRITE (0,*) "nbr_cells: " , myDim_nod2D
+    WRITE (0,*) "nbr_connections: " , nbr_connections
+
+    ALLOCATE(x_vertices(nbr_vertices))
+    ALLOCATE(y_vertices(nbr_vertices))
+    ALLOCATE(nbr_vertices_per_cell(myDim_nod2D))
     ALLOCATE(cell_to_vertex(nbr_connections))
-    k = 1
-    DO i = 1, myDim_nod2D
-       DO j = 1, nod_in_elem2D_num(i)
-          cell_to_vertex(k) = nod_in_elem2D(j, i)
-          k = k + 1
+
+    ! compute vertices
+    ! 1 element centers
+    DO i=1,myDim_elem2D
+       x_vertices(i) = geo_coord_nod2D(1,elem2D_nodes(1, i))
+       y_vertices(i) = (1./3.)*SUM(geo_coord_nod2D(2,elem2D_nodes(1:3, i)))
+       DO j=2,3
+          delta = geo_coord_nod2D(1,elem2D_nodes(j, i)) - geo_coord_nod2D(1,elem2D_nodes(1, i))
+          x_vertices(i) = x_vertices(i) + (1./3.)*(MODULO(delta + pi, 2*pi) - pi)
        END DO
     END DO
+    ! 2 edges midpoints
+    DO i=1,myDim_edge2D
+       delta = geo_coord_nod2D(1,edges(2,i)) - geo_coord_nod2D(1,edges(1,i))
+       x_vertices(myDim_elem2D + i) = geo_coord_nod2D(1,edges(1,i)) + 0.5*(MODULO(delta + pi, 2*pi) - pi)
+       y_vertices(myDim_elem2D + i) = 0.5*(geo_coord_nod2D(2,edges(1,i)) + geo_coord_nod2D(2,edges(2,i)))
+    END DO
+    ! 3 boundary nodes -> are added on the fly when the cells are computed
 
-    CALL yac_fdef_grid_nonuniform_r8("fesom_grid", &
-         partit%myDim_elem2D, &  ! integer
-         partit%myDim_nod2D, &   ! integer
-         nbr_connections, &
-         nod_in_elem2D_num(1:partit%myDim_nod2D), &
-         elem_x, &
-         elem_y, &
+    ! compute cells
+    vtx_idx = myDim_elem2D + myDim_edge2D
+    c2v_idx = 0
+    DO i=1,myDim_nod2D
+       nbr_vertices_per_cell(i) = 2*nod_in_elem2D_num(i)
+       curr_elem = nod_in_elem2D(1,i)
+       curr_edge = elem_edges(1, curr_elem)
+       IF (ALL(edges(1:2,curr_edge) /= i)) curr_edge = elem_edges(2, curr_elem)
+       IF (node_is_boundary(i)) THEN
+          nbr_vertices_per_cell(i) = nbr_vertices_per_cell(i) + 2
+          ! we're starting with the boundary node
+          vtx_idx = vtx_idx+1
+          x_vertices(vtx_idx) = geo_coord_nod2D(1,i)
+          y_vertices(vtx_idx) = geo_coord_nod2D(2,i)
+          c2v_idx = c2v_idx + 1
+          cell_to_vertex(c2v_idx) = vtx_idx
+          elem_loop: DO j=1,nod_in_elem2D_num(i)
+             DO k=1,3
+                IF (myList_edge2D(elem_edges(k,nod_in_elem2D(j,i))) > edge2D_in .AND. &
+                     ANY(edges(1:2,elem_edges(k,nod_in_elem2D(j,i))) == i)) THEN
+                   curr_elem = nod_in_elem2D(j,i)
+                   curr_edge = elem_edges(k, curr_elem)
+                   EXIT elem_loop
+                END IF
+             END DO
+          END DO elem_loop
+       END IF
+
+       DO j=1,nod_in_elem2D_num(i)
+          ! add the midpoint of curr_edge
+          c2v_idx = c2v_idx + 1
+          cell_to_vertex(c2v_idx) = myDim_elem2D + curr_edge
+          ! add the center of curr_elem
+          c2v_idx = c2v_idx + 1
+          IF (curr_elem < 1) THEN
+             WRITE (0,*) "elem 0 detected in cell ", i, " with ", nbr_vertices_per_cell(i), " vertices - j is ", j, " is_boundary ", node_is_boundary(i)
+          END IF
+          cell_to_vertex(c2v_idx) = curr_elem
+          ! find next edge
+          edge_loop: DO k=1,3
+             IF (elem_edges(k, curr_elem) /= curr_edge .AND. &
+                  ANY(edges(1:2,elem_edges(k, curr_elem)) == i)) THEN
+                curr_edge = elem_edges(k, curr_elem)
+                EXIT edge_loop
+             END IF
+          END DO edge_loop
+          IF (edge_tri(1, curr_edge) /= curr_elem) THEN
+             curr_elem = edge_tri(1, curr_edge)
+          ELSE
+             curr_elem = edge_tri(2, curr_edge)
+          END IF
+       END DO
+
+       ! in case of a boundary volume, we add the others edges midpoint
+       IF (node_is_boundary(i)) THEN
+          ! add the midpoint of curr_edge
+          c2v_idx = c2v_idx + 1
+          cell_to_vertex(c2v_idx) = myDim_elem2D + curr_edge
+       END IF
+    END DO
+
+    WRITE (0,*) "c2v_idx: ", c2v_idx
+
+    CALL yac_fdef_grid_nonuniform_r8( &
+         "fesom_grid", & ! grid_name
+         nbr_vertices, &   ! nbr_vertices
+         myDim_nod2D, &  ! nbr_cells
+         nbr_connections, & ! nbr_connections
+         nbr_vertices_per_cell, & ! nbr_vertices_per_cell
+         x_vertices, & ! x_vertices
+         y_vertices, & ! y_vertices
          cell_to_vertex, &
          grid_id)
 
     CALL yac_fdef_points(grid_id, &
-         partit%myDim_nod2D, &
+         myDim_nod2D, &
          YAC_LOCATION_CELL, &
-         geo_coord_nod2D(1, 1:partit%myDim_nod2D), &
-         geo_coord_nod2D(2, 1:partit%myDim_nod2D), &
+         geo_coord_nod2D(1,1:myDim_nod2D), &
+         geo_coord_nod2D(2,1:myDim_nod2D), &
          points_id)
 
     cpl_send( 1)='sst_feom' ! 1. sea surface temperature [°C]      ->
