96a95deff3df6ac4fcb06861fab8d51a438c3d23
mandresm <miguel.andres-martinez@awi.de>
2023-09-04 16:50:02 +0200

    adds the possibility of specifying the compiler as the input of configure

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source env.sh # source this from your run script too
+source env.sh $1 # source this from your run script too
 mkdir build || true # make sure not to commit this to svn or git
 cd build
 cmake .. $@ -DCMAKE_BUILD_TYPE=Debug # not required when re-compiling
