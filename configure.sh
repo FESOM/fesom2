9e23e43f2c6a3b0c274ad3bfda2ea280433cf9e4
suvarchal <suvarchal.kumar@gmail.com>
2023-10-11 22:50:42 +0200

    add needed changes to configure.sh from wdeconinck/feature/cmake-improvements

--- configure.sh
+++ configure.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-
+HERE=$PWD
 source env.sh # source this from your run script too
-mkdir build || true # make sure not to commit this to svn or git
+mkdir -p build
 cd build
-cmake .. $@ -DCMAKE_BUILD_TYPE=Debug # not required when re-compiling
-                                     # additional cmake arguments can be passed to configure.sh
-				     # this also includes fesom specific options in CMakeLists, can be used as -DFESOM_COUPLED=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=$HERE -DCMAKE_BUILD_TYPE=Debug ${CMAKE_ARGS} $@
+  # not required when re-compiling
+  # additional cmake arguments can be passed to configure.sh
+  # this also includes fesom specific options in CMakeLists, can be used as -DFESOM_COUPLED=ON
 make install -j`nproc --all`
