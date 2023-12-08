0ff129fe4922c58c4d5400750f382a57558f871a
Patrick <Patrick.Scholz@awi.de>
2023-12-08 11:15:51 +0100

    fix bug in the vertical tracer gradient that is used in the horizontal REDI diffusivity. Before we used the new hnode to compute the gradient but it has to be done on the old hnode

--- src/oce_tracer_mod.F90
+++ src/oce_tracer_mod.F90
@@ -116,18 +116,14 @@ SUBROUTINE tracer_gradient_z(ttf, partit, mesh)
 #include "associate_mesh_ass.h" 
 !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(n, nz, nzmin, nzmax, dz)
     DO n=1, myDim_nod2D+eDim_nod2D
-    !!PS nlev=nlevels_nod2D(n)
-    nzmax=nlevels_nod2D(n)
-    nzmin=ulevels_nod2D(n)
-    !!PS DO nz=2,  nlev-1
-    DO nz=nzmin+1,  nzmax-1
-        dz=0.5_WP*(hnode_new(nz-1,n)+hnode_new(nz,n))
-        tr_z(nz, n)=(ttf(nz-1,n)-ttf(nz,n))/dz
-    END DO
-    !!PS tr_z(1,    n)=0.0_WP
-    !!PS tr_z(nlev, n)=0.0_WP
-    tr_z(nzmin, n)=0.0_WP
-    tr_z(nzmax, n)=0.0_WP
+        nzmax=nlevels_nod2D(n)
+        nzmin=ulevels_nod2D(n)
+        DO nz=nzmin+1,  nzmax-1
+            dz=0.5_WP*(hnode(nz-1,n)+hnode(nz,n))
+            tr_z(nz, n)=(ttf(nz-1,n)-ttf(nz,n))/dz
+        END DO
+        tr_z(nzmin, n)=0.0_WP
+        tr_z(nzmax, n)=0.0_WP
     END DO
 !$OMP END PARALLEL DO
 END SUBROUTINE tracer_gradient_z
