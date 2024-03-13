f154aefc2b31c353808f3301612382f0df717211
Jan Streffing <jan.streffing@awi.de>
2024-03-13 10:35:36 +0100

    Update gen_model_setup.F90

--- src/gen_model_setup.F90
+++ src/gen_model_setup.F90
@@ -28,9 +28,8 @@ subroutine setup_model(partit)
   read (fileunit, NML=geometry)
   read (fileunit, NML=calendar)
   read (fileunit, NML=run_config)
-#if defined (__async_icebergs)
-    read (fileunit,NML=icebergs)
-#endif
+  read (fileunit,NML=icebergs)
+
 !!$  read (fileunit, NML=machine)
   close (fileunit)
   
