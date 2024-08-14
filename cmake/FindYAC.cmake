5b7023441672180668b2b5f73d3ec640e1c098ef
Nils-Arne Dreier <dreier@dkrz.de>
2024-08-14 08:38:40 +0200

    fix: adapt yac for version 3.0.3

--- cmake/FindYAC.cmake
+++ cmake/FindYAC.cmake
@@ -10,9 +10,10 @@ PKG_CHECK_MODULES(PC_yac yac)
 find_path(YAC_Fortran_INCLUDE_DIRECTORIES mo_yac_finterface.mod HINTS ${PC_yac_INCLUDE_DIRS} ${yac_DIR}/src)
 
 find_library(YAC_LIBRARY yac HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/src)
-find_library(YAC_CLAPACK_LIBRARY yac_clapack HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/clapack)
-find_library(YAC_MTIME_LIBRARY yac_mtime HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/mtime)
+#find_library(YAC_CLAPACK_LIBRARY yac_clapack HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/clapack)
+#find_library(YAC_MTIME_LIBRARY yac_mtime HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/mtime)
 
-list(REMOVE_ITEM PC_yac_LINK_LIBRARIES yac yac_clapack yac_mtime)
+#list(REMOVE_ITEM PC_yac_LINK_LIBRARIES yac yac_clapack yac_mtime)
 
-set(YAC_Fortran_LIBRARIES ${YAC_LIBRARY} ${YAC_CLAPACK_LIBRARY} ${YAC_MTIME_LIBRARY} ${PC_yac_LINK_LIBRARIES} ${PC_yac_LDFLAGS_OTHER})
+#message(FATAL_ERROR ${PC_yac_LDFLAGS})
+set(YAC_Fortran_LIBRARIES "-L${yac_DIR}/src" ${PC_yac_LDFLAGS})
