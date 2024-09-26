817feb8e9edb108b4168e82eee9ab1957758a750
Juha Tonttila <juha.tonttila@csc.fi>
2024-09-26 12:50:28 +0300

    Forgot configure.sh, returned to original.

--- configure.sh
+++ configure.sh
@@ -6,12 +6,12 @@ HERE=$PWD
 SOURCE_DIR="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd -P )"
 BUILD_DIR=${BUILD_DIR:-build}
 
-#source env.sh # source this from your run script too
+source env.sh # source this from your run script too
 mkdir -p ${BUILD_DIR} # make sure not to commit this to svn or git
 cd ${BUILD_DIR}
 cmake ${SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=$HERE -DCMAKE_BUILD_TYPE=Debug ${CMAKE_ARGS} $@
     # not required when re-compiling
     # additional cmake arguments can be passed to configure.sh
     # this also includes fesom specific options in CMakeLists, can be used as -DFESOM_COUPLED=ON
-#make install -j`nproc --all`
+make install -j`nproc --all`
 
