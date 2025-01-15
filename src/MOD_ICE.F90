37f9a9742915537ffc082404ef9a57a93c705091
Jan Streffing <jan.streffing@awi.de>
2025-01-15 23:56:21 +0100

    remove ifs/openifs ice thermo coupling changes

--- src/MOD_ICE.F90
+++ src/MOD_ICE.F90
@@ -93,7 +93,7 @@ TYPE T_ICE_ATMCOUPL
     real(kind=WP), allocatable, dimension(:)    :: oce_flx_h, ice_flx_h, tmpoce_flx_h, tmpice_flx_h
 #if defined (__oifs) || defined (__ifsinterface)
     !___________________________________________________________________________
-    real(kind=WP), allocatable, dimension(:)    :: ice_alb, enthalpyoffuse, flx_qres, flx_qcon
+    real(kind=WP), allocatable, dimension(:)    :: ice_alb, enthalpyoffuse
     ! !!! DONT FORGET ice_temp rhs_tempdiv rhs_temp is advected for oifs !!! --> becomes additional ice
     ! tracer in ice%data(4)%values
 #endif /* (__oifs)  */
@@ -664,7 +664,7 @@ subroutine ice_init(ice, partit, mesh)
     allocate(ice%flx_h( node_size))
     ice%flx_fw           = 0.0_WP
     ice%flx_h            = 0.0_WP
-    
+
     !___________________________________________________________________________
     ! initialse data array of ice derived type containing "ice tracer" that have
     ! to be advected: a_ice (index=1), m_ice (index=2), m_snow (index=3),
@@ -752,10 +752,6 @@ subroutine ice_init(ice, partit, mesh)
     allocate(ice%atmcoupl%enthalpyoffuse(node_size))
     ice%atmcoupl%ice_alb       = 0.6_WP
     ice%atmcoupl%enthalpyoffuse= 0.0_WP
-    allocate(ice%atmcoupl%flx_qres(node_size))
-    allocate(ice%atmcoupl%flx_qcon(node_size))
-    ice%atmcoupl%flx_qres      = 0.0_WP    
-    ice%atmcoupl%flx_qcon      = 0.0_WP
 #endif /* (__oifs) */
 #endif /* (__oasis) */
 
