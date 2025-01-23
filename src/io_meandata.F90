53e92d15b4427a13a286e72dedca6d7402138a49
Sebastian Beyer <sebastian.beyer@awi.de>
2025-01-23 20:46:21 +0100

    Rename heatcontent variables
    I did misunderstand the way to create streams before and used different
    names for the name in the namelist and the name in the output. Now it
    matches:
    
    heatcontent300m -> hc300m
    heatcontent700m -> hc700m
    heatcontent     -> hc

--- src/io_meandata.F90
+++ src/io_meandata.F90
@@ -366,15 +366,15 @@ CASE ('MLD3      ')
 ! output heat content (for destine)
 CASE ('hc300m')
     if (ldiag_destinE) then
-        call def_stream(nod2D, myDim_nod2D, 'heatcontent300m', 'Vertically integrated heat content upper 300m',   'J m**-2', heatcontent(1,1:myDim_nod2D), io_list(i)%freq, io_list(i)%unit, io_list(i)%precision, partit, mesh)
+        call def_stream(nod2D, myDim_nod2D, 'hc300m', 'Vertically integrated heat content upper 300m',   'J m**-2', heatcontent(1,1:myDim_nod2D), io_list(i)%freq, io_list(i)%unit, io_list(i)%precision, partit, mesh)
     end if 
 CASE ('hc700m')
     if (ldiag_destinE) then
-        call def_stream(nod2D, myDim_nod2D, 'heatcontent700m', 'Vertically integrated heat content upper 700m',   'J m**-2', heatcontent(2,1:myDim_nod2D), io_list(i)%freq, io_list(i)%unit, io_list(i)%precision, partit, mesh)
+        call def_stream(nod2D, myDim_nod2D, 'hc700m', 'Vertically integrated heat content upper 700m',   'J m**-2', heatcontent(2,1:myDim_nod2D), io_list(i)%freq, io_list(i)%unit, io_list(i)%precision, partit, mesh)
     end if 
 CASE ('hc')
     if (ldiag_destinE) then
-        call def_stream(nod2D, myDim_nod2D, 'heatcontent',     'Vertically integrated heat content total column', 'J m**-2', heatcontent(3,1:myDim_nod2D), io_list(i)%freq, io_list(i)%unit, io_list(i)%precision, partit, mesh)
+        call def_stream(nod2D, myDim_nod2D, 'hc',     'Vertically integrated heat content total column', 'J m**-2', heatcontent(3,1:myDim_nod2D), io_list(i)%freq, io_list(i)%unit, io_list(i)%precision, partit, mesh)
     end if 
 !_______________________________________________________________________________
 !---wiso-code
