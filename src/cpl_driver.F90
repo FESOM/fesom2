5e7b5b09fb05015e3ed10e9c2ed97748441f04ce
Tido Semmler <tido.semmler@met.ie>
2025-02-25 16:35:18 +0000

    make omip fluxes (hydrology and enthalpy) consistent with runoff mapper fluxes

--- src/cpl_driver.F90
+++ src/cpl_driver.F90
@@ -421,7 +421,7 @@ contains
     cpl_recv(11) = 'heat_swo'    
     cpl_recv(12) = 'hydr_oce'
     cpl_recv(13) = 'enth_oce'
-    cpl_recv(14) = 'calv_for'
+    cpl_recv(14) = 'hydr_for'
     cpl_recv(15) = 'enth_for'
 #else
     cpl_recv(1)  = 'taux_oce'
