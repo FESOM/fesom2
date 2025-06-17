108b15223ee997e06614198a317b897bfd0b4f88
seshadri levante <seshadri.basava@awi.de>
2025-06-17 16:54:49 +0200

    Compiling and running base OpenACC code with nvhpc.

--- src/ice_fct.F90
+++ src/ice_fct.F90
@@ -570,10 +570,12 @@ subroutine ice_fem_fct(tr_array_id, ice, partit, mesh)
 #endif
     ! Auxiliary elemental operator (mass matrix- lumped mass matrix)
 
+    ! do we need to make the entire array of icoef equal to 1 ?
+    ! if so, we have to write another loop for that. For now, I am running it on cpu.
+    icoef = 1
 #ifdef ENABLE_OPENACC
     !$ACC PARALLEL LOOP GANG VECTOR DEFAULT(PRESENT)
 #endif
-    icoef = 1
     do n=1,3   ! three upper nodes
         ! Cycle over rows  row=elnodes(n)
         icoef(n,n)=-2
