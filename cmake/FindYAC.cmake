9bf6052d1839f221045078259efe2db7b1071f6d
Nils-Arne Dreier <dreier@dkrz.de>
2025-10-15 10:15:50 +0200

    build(yac): more adaptations to FindYAC.cmake for newer yac versions

--- cmake/FindYAC.cmake
+++ cmake/FindYAC.cmake
@@ -12,13 +12,13 @@ if ( NOT yac_FOUND )
   PKG_CHECK_MODULES(PC_yac yac-mci)
 endif()
 
-find_path(YAC_Fortran_INCLUDE_DIRECTORIES mo_yac_finterface.mod HINTS ${PC_yac_INCLUDE_DIRS} ${yac_DIR}/src/mci)
+find_path(YAC_Fortran_INCLUDE_DIRECTORIES yac.mod mo_yac_finterface.mod HINTS ${PC_yac_INCLUDE_DIRS} ${yac_DIR}/src/mci)
 
-find_library(YAC_LIBRARY yac HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/src)
+find_library(YAC_LIBRARY yac yac_mci HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/src/mci ${yac_DIR}/src)
 #find_library(YAC_CLAPACK_LIBRARY yac_clapack HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/clapack)
 #find_library(YAC_MTIME_LIBRARY yac_mtime HINTS ${PC_yac_LINK_LIBRARIES} ${yac_DIR}/mtime)
 
 #list(REMOVE_ITEM PC_yac_LINK_LIBRARIES yac yac_clapack yac_mtime)
 
 #message(FATAL_ERROR "${PC_yac_LDFLAGS}")
-set(YAC_Fortran_LIBRARIES "-L${yac_DIR}/src" ${PC_yac_LDFLAGS})
+set(YAC_Fortran_LIBRARIES "-L${yac_DIR}/src" "-L${yac_DIR}/src/mci" ${PC_yac_LDFLAGS})
