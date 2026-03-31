85498ee150c0f34fc2a60240fa305de61650adc3
ackerlar <lars.ackermann@awi.de>
2026-03-31 21:25:48 +0200

    rm src/cvmix_driver

--- src/cvmix_driver/CVMix-build/CMakeFiles/cvmix_shared.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcvmix.pdb"
-  "libcvmix.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang Fortran)
-  include(CMakeFiles/cvmix_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
